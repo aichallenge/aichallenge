691a2d6ffb181090843956a97b64a700c5e17225
Jeff Cameron <cameron.jp@gmail.com>
2010-05-06 01:30:51 +0000

    Adding a planet class to the Galcon engine.
    git-svn-id: https://ai-contest.googlecode.com/svn/trunk@62 9d0e4ec5-6d14-e29b-3149-cabf4038ef1e

---
+++ galcon/engine/planet.cc
@@ -0,0 +1,57 @@
+// Copyright 2010 owners of the AI Challenge project
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy
+// of the License at http://www.apache.org/licenses/LICENSE-2.0 . Unless
+// required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//
+// Author: Jeff Cameron (jeff@jpcameron.com)
+
+#include "galcon/engine/planet.h"
+
+Planet::Planet(int owner, int num_ships, int growth_rate, double x, double y) {
+  owner_ = owner;
+  num_ships_ = num_ships;
+  growth_rate_ = growth_rate;
+  x_ = x;
+  y_ = y;
+}
+
+int Planet::Owner() const {
+  return owner_;
+}
+
+int Planet::NumShips() const {
+  return num_ships_;
+}
+
+int Planet::GrowthRate() const {
+  return growth_rate_;
+}
+
+double Planet::X() const {
+  return x_;
+}
+
+double Planet::Y() const {
+  return y_;
+}
+
+void Planet::Owner(int new_owner) {
+  owner_ = new_owner;
+}
+
+void Planet::NumShips(int new_num_ships) {
+  num_ships_ = new_num_ships;
+}
+
+void Planet::AddShips(int amount) {
+  num_ships_ += amount;
+}
+
+void Planet::RemoveShips(int amount) {
+  num_ships_ -= amount;
+}
