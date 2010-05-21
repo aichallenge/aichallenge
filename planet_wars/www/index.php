c43fd0f95e5c6c88e7e19e62d481e86b73d47ab0
Jeff Cameron <cameron.jp@gmail.com>
2010-05-21 03:43:32 +0000

    Adding a placeholder index.php.
    git-svn-id: https://ai-contest.googlecode.com/svn/trunk@81 9d0e4ec5-6d14-e29b-3149-cabf4038ef1e

---
+++ planet_wars/www/index.php
@@ -0,0 +1,7 @@
+<?php include 'header.php'; ?>
+
+<h2>The AI Challenge is Back!</h2>
+<p>Welcome back. Did you miss it? Now you can all neglect your families again!
+  </p>
+
+<?php include 'header.php'; ?>
