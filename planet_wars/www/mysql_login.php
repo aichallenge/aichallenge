b12baa81f0225ccea0b2762f4c9f760839a81cd8
Jeff Cameron <cameron.jp@gmail.com>
2010-05-28 20:59:43 +0000

    Taking the database credentials out of the source code.
    git-svn-id: https://ai-contest.googlecode.com/svn/trunk@98 9d0e4ec5-6d14-e29b-3149-cabf4038ef1e

--- planet_wars/www/mysql_login.php
+++ planet_wars/www/mysql_login.php
@@ -4,11 +4,15 @@
 if (!isset($JPC_CONTEST_MYSQL_LOGIN_PHP__)) {
 $JPC_CONTEST_MYSQL_LOGIN_PHP__ = 1;
 
+// Get the database login information from the server_info.txt file.
+include 'server_info.php';
+$server_info = get_server_info();
+
 // Login credentials for MySQL database.
-$db_host = "127.0.0.1"; // Host name
-$db_username = "j3camero"; // Mysql username
-$db_password = "nxjGRAeqLQ"; // Mysql password
-$db_name = "j3camero_contest"; // Database name
+$db_host = $server_info["db_host"]; // Host name
+$db_username = $server_info["db_username"]; // Mysql username
+$db_password = $server_info["db_password"]; // Mysql password
+$db_name = $server_info["db_name"]; // Database name
 
 // Connect to server and select database.
 mysql_connect($db_host, $db_username, $db_password) or die('cannot connect: ' . print_r(error_get_last()));
