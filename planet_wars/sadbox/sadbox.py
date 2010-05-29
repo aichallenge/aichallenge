3e53701212a03499acc657cc6e1691599748efa0
S. S. Albiz <ssalbiz@csclub.uwaterloo.ca>
2010-05-29 06:00:05 +0000

    Fixes for ssh auth, multiple instances on single host, refactoring changes
    git-svn-id: https://ai-contest.googlecode.com/svn/trunk@111 9d0e4ec5-6d14-e29b-3149-cabf4038ef1e

--- planet_wars/sadbox/sadbox.py
+++ planet_wars/sadbox/sadbox.py
@@ -3,32 +3,72 @@ import os
 import time
 import signal
 import sys
+import getopt
+import subprocess
+import shlex
 
-sadbox_path = sys.argv[1]
-sadbox_dir = os.path.basename(sadbox_path)
-img_name = "test.img"
-qemu_cmd = "/usr/bin/qemu"
-qemu_netopt = "-net" 
-qemu_nicopt = "nic" 
-qemu_hostfwd = "user,hostfwd=tcp:127.0.0.1:5555-:22"
-qemu_identfile = "vmkey"
-dest_path = "contestvm@localhost:~"
-cmd = "cd ./" + sadbox_dir + "/;" +  sys.argv[2]
+def copy_exec_dir(qemu_port, qemu_identfile, sadbox_path, dest_path):
+	scp_cmd = "scp -r -P " + str(qemu_port) + " -i " + qemu_identfile + " " + sadbox_path + " " + dest_path
+	scp_args = shlex.split(scp_cmd)
+	scp_process = subprocess.Popen(scp_args, close_fds=True)
+	print "copying files to instance"
+	scp_process.wait()
+	return scp_process
+#...
 
-vmpid = os.fork()
-if (vmpid == 0):
-	os.execl(qemu_cmd, qemu_cmd, img_name, qemu_netopt, qemu_nicopt, qemu_netopt, qemu_hostfwd)
-print "spinning up qemu instance"
-time.sleep(15) #allow time for vm to spin up
-pid = os.fork()
-if (pid == 0):
-	os.execlp("scp", "", "-r", "-P 5555", "-i", qemu_identfile, sadbox_path, dest_path)
-print "copying files to instance"
-os.wait()
-pid = os.fork()
-if (pid == 0):
-	os.execlp("ssh", "", "-p 5555", "-i",  qemu_identfile, "contestvm@localhost", cmd)
-print "executing command inside VM"
-os.wait() #wait for command to finish execution
-os.kill(vmpid, signal.SIGINT)
-os.wait() #wait for vm to die
+
+
+def launch_qemu():
+	img_name = "test.img"
+	qemu_port = 5555
+        qemu_cmd = "/usr/bin/qemu " + img_name + " -net nic -net user,hostfwd=tcp:127.0.0.1:" + str(qemu_port) + "-:22 -nographic"
+	qemu_args = shlex.split(qemu_cmd)
+	print "spinning up qemu instance"
+	qemu_process = subprocess.Popen(qemu_args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True)
+	time.sleep(1)
+	qemu_process.poll
+	while qemu_process.returncode != None and qemu_port < 5565:
+		qemu_port += 1
+        	qemu_cmd = "/usr/bin/qemu " + img_name + " -net nic -net user,hostfwd=tcp:127.0.0.1:" + str(qemu_port) + "-:22 -nographic"
+		qemu_args = shlex.split(qemu_cmd)
+		qemu_process = subprocess.Popen(qemu_args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True)
+		time.sleep(1)
+	time.sleep(10) #allow time for vm to spin up
+	return (qemu_process, qemu_port)
+#...
+
+def main():
+	qemu_identfile = "sshkey/vmkey"
+	dest_path = "contestvm@localhost:~"
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "d:c:", ["directory=", "command="])
+	except getopt.GetoptError, err:
+		print str(err)
+		sys.exit(2)
+	sadbox_path = None
+	cmd = None
+	for o, a in opts:
+		if o in ("-d", "--directory"):
+			sadbox_path = a
+		elif o in ("-c", "--command"):
+			cmd = a
+		else:
+			print o, a
+	sadbox_dir = os.path.basename(sadbox_path)
+	cmd = "cd ./" + sadbox_dir + "/; " + cmd
+	print sadbox_path, sadbox_dir, cmd
+	(qemu_proc, port) = launch_qemu()
+	scp_proc = copy_exec_dir(port, qemu_identfile, sadbox_path, dest_path)
+	child_cmd = "ssh -p " + str(port) + " -i " + qemu_identfile + " contestvm@localhost " + cmd
+	child_args = shlex.split(child_cmd)
+	child_process = subprocess.Popen(child_args, close_fds=True)
+	print "executing command inside VM"
+
+	child_process.wait()
+	#wait for command to finish execution
+	os.kill(qemu_proc.pid, signal.SIGINT)
+	qemu_proc.wait() #wait for vm to die
+#...
+
+if __name__ == "__main__":
+	main()
