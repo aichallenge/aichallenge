0f04be0320555c1104af374d55fa2a95d0d7b800
Jeff Cameron <cameron.jp@gmail.com>
2010-05-30 01:21:35 +0000

    Getting rid of the old compile pipeline. Good riddance\!
    git-svn-id: https://ai-contest.googlecode.com/svn/trunk@117 9d0e4ec5-6d14-e29b-3149-cabf4038ef1e

--- planet_wars/compile_daemon/compile_for_a_bit.sh
+++
@@ -1,6 +0,0 @@
-date
-for i in {1..12}
-do
-  nice /users/contest2/tron/submission_pipeline/compile_one_new_entry.sh;
-  sleep 5;
-done
--- planet_wars/compile_daemon/scp_for_a_bit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in {1..5}
-do
-  python ~/tron/submission_pipeline/scp_new_submissions.py
-  sleep 10
-done
