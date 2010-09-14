75e7e547316703c24e5f66cafd3c7959dc6d4f0a
Daniel Von Fange <daniel@leancoder.com>
2010-09-14 14:32:03 +0000

    Merge: 20100914-play_compressed_playbacks
    git-svn-id: https://ai-contest.googlecode.com/svn/trunk@343 9d0e4ec5-6d14-e29b-3149-cabf4038ef1e

--- planet_wars/www/game_info.php
+++ planet_wars/www/game_info.php
@@ -2,21 +2,48 @@
 
 include "mysql_login.php";
 
-$game_id = mysql_real_escape_string($_GET["game_id"]);
-if (strlen($game_id) > 0) {
-  $query = file_get_contents("game_info_query.sql") . $game_id;
-  $result = mysql_query($query);
-  if ($result) {
-    if ($row = mysql_fetch_assoc($result)) {
-      foreach ($row as $key => $value) {
-        echo "$key=$value\n";
+$game_id = (int)($_GET["game_id"]);
+
+if($game_id == 0){
+  echo "No game_id given.\n";
+}else{
+    $query = "SELECT
+      g.*,
+      u1.username AS player_one,
+      u2.username AS player_two,
+      g.player_one as player_one_id,
+      g.player_two as player_two_id
+    FROM games g
+    INNER JOIN submissions s1 ON g.player_one = s1.submission_id
+    INNER JOIN users u1 ON s1.user_id = u1.user_id
+    INNER JOIN submissions s2 ON g.player_two = s2.submission_id
+    INNER JOIN users u2 ON s2.user_id = u2.user_id
+    WHERE g.game_id = $game_id";
+    $result = mysql_query($query);
+    if(!$result){
+      echo "Could not query the database.\n";
+    }else{
+      $row = mysql_fetch_assoc($result);
+      if($playback_string = get_playback($game_id)){
+        $row['playback_string'] = $playback_string;
+        $row['using_compression'] = true;
+      }
+      if($row){
+        foreach ($row as $key => $value) {
+          echo "$key=$value\n";
+        }
       }
     }
-  } else {
-    echo "Could not query the database.\n";
+}
+
+
+function get_playback($game_id){
+  $sql = "SELECT * from playback where game_id = $game_id";
+  $q = mysql_query($sql);
+  $result = mysql_fetch_assoc($q);
+  if($result['playback_string']!=''){
+    return gzuncompress($result['playback_string']);
   }
-} else {
-  echo "No game_id given.\n";
 }
 
 ?>
