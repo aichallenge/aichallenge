60abda8993b641c6a0f5cc76d87afc3022b27b21
Daniel Von Fange <daniel@leancoder.com>
2010-09-30 10:45:06 +0000

    Cloud game server tournament manager.
    git-svn-id: https://ai-contest.googlecode.com/svn/branches/20100929-games-in-the-cloud@393 9d0e4ec5-6d14-e29b-3149-cabf4038ef1e

--- planet_wars/backend/tournament_manager.py
+++ planet_wars/backend/tournament_manager.py
@@ -1,14 +1,20 @@
 import engine
+from server_info import server_info
+from compile_anything import compile_function
+
 import logging
 import logging.handlers
 import MySQLdb
 import os
 import random
-from server_info import server_info
 import subprocess
 import sys
+import signal
 import time
 import zlib
+import urllib
+import simplejson as json
+import traceback
 
 # Set up logging
 logger = logging.getLogger('tm_logger')
@@ -20,7 +26,7 @@ logger.addHandler(handler)
 tm_pid = os.getpid()
 
 def log_message(message):
-  logger.info(str(tm_pid) + ": " + message)
+  logger.info(str(tm_pid) + ": %s" % message)
   print message
 
 log_message("started. time is " + str(time.time()))
@@ -31,108 +37,95 @@ if len(sys.argv) != 2:
   sys.exit(1)
 time_limit = int(sys.argv[1])
 log_message("time_limit: " + str(time_limit))
-log_message("connecting to db")
 
-# Connect to the database.
-connection = MySQLdb.connect(host = server_info["db_host"],
-                             user = server_info["db_username"],
-                             passwd = server_info["db_password"],
-                             db = server_info["db_name"])
-cursor = connection.cursor(MySQLdb.cursors.DictCursor)
-log_message("db connection established")
-log_message("getting active submissions from db")
 
-# Get the list of currently active submissions.
-submissions_sql = """
-  SELECT s.*, l.*
-  FROM submissions s
-  INNER JOIN (
-    SELECT
-      MAX(submission_id) AS submission_id
-    FROM submissions
-    GROUP BY user_id
-  ) AS o ON o.submission_id = s.submission_id
-  INNER JOIN languages l ON l.language_id = s.language_id
-  WHERE s.status = 40
-"""
-cursor.execute(submissions_sql)
-submissions = list(cursor.fetchall()) 
-log_message("found " + str(len(submissions)) + " active submissions")
-# Get the list of maps.
-log_message("fetching map list from db")
-cursor.execute("SELECT * FROM maps")
-maps = cursor.fetchall()
-log_message("found " + str(len(maps)) + " maps")
-# Get the rankings
-cursor.execute("""
-  SELECT r.* from rankings r WHERE leaderboard_id = (select max(leaderboard_id) from leaderboards);
-""")
-rankings = cursor.fetchall()
-log_message("found " + str(len(maps)) + " rankings")
-# Order submissions by ranking
-rankings_by_submission_id = {}
-for ranking in rankings:
-  rankings_by_submission_id[ranking['submission_id']] = ranking['rank']
-default_rank = int(len(rankings)/2)
-for submission in submissions:
-  submission['rank'] = rankings_by_submission_id.get(submission['submission_id'],default_rank)
-submissions.sort(key=lambda x: x['rank'])
-# Are there enough players? Are there enough maps?
-if len(submissions) < 2:
-  log_message("There are fewer than two active submissions. No games can " + \
-    "be played")
-  sys.exit(0)
-if len(maps) < 1:
-  log_message("There are no maps in the database. No games can be played")
-  sys.exit(0)
-log_message("starting tournament. time is " + str(time.time()))
+class GameAPIClient:
+  def __init__(self, base_url, api_key):
+    self.base_url = base_url
+    self.api_key = api_key
+  
+  def get_matchup(self):
+    url = self.base_url+'/api_get_matchup.php'
+    url += '?api_key=%s' % self.api_key
+    data = urllib.urlopen(url).read()
+    # log_message(data)
+    return json.loads(data)
+  
+  def ensure_submission_is_local(self, submission_id, language_name, platform_specific_compilation):
+    submission_id = int(submission_id)
+    submission_dir = "../submissions/%d" % submission_id
+    if os.path.exists(submission_dir):
+      log_message("Submission %d is local" % submission_id)
+      return
+    log_message("Downloading %d " % submission_id)
+    os.system("mkdir %s" % submission_dir);
+    url = self.base_url+'/api_get_submission.php'
+    url += '?api_key=%s&submission_id=%d' % (self.api_key, submission_id)
+    # log_message(url)
+    os.system("cd %s; curl --silent '%s' | tar -xz" % (submission_dir, url));
+    
+    if int(platform_specific_compilation) == 1:      # This has a race condition in that two workers
+      log_message("Compiling %s " % submission_id)   # could be simultaniously trying to compile the same 
+      os.chdir(submission_dir)                       # bot. Or one could try to run a not yet compiled bot. 
+      out, err = compile_function(language_name)
+      os.chdir('../../backend')
+      if len(err) > 0:
+        log_message(err)
+        raise Exception()
+    else:
+      log_message("Not compiling %s " % submission_id)
+    
+    
+    
+  def record_game(self, data):
+    url = self.base_url+'/api_record_game.php'
+    url += '?api_key=%s' % self.api_key
+    print urllib.urlopen(url,json.dumps(data)).read()
+
+
+cloud = GameAPIClient( server_info['api_base_url'], server_info['api_key'])
+
+  
+
 start_time = time.time()
 while time.time() - start_time < time_limit:
   log_message("attempting to match two bots. time is " + str(time.time()))
-  map = random.choice(maps)
-  # Choose a bot that has not been played recently
-  next_bot_sql = submissions_sql + " ORDER BY last_game_timestamp LIMIT 1"
-  cursor = connection.cursor(MySQLdb.cursors.DictCursor)
-  cursor.execute(next_bot_sql)
-  player_one = cursor.fetchone()
-  update_timestamp_sql = "UPDATE submissions SET last_game_timestamp = current_timestamp WHERE submission_id = %s"
-  cursor.execute(update_timestamp_sql, player_one['submission_id'])
-  player_one['rank'] = rankings_by_submission_id.get(player_one['submission_id'],default_rank)
-  # Choose a probably near bot to fight
-  pickable_spread = random.choice([5,10,10,20,40,100,200,1000])
-  player_two = player_one
-  while player_one['user_id'] == player_two['user_id']:
-    if player_one in submissions:
-      player_one_offset = submissions.index(player_one)
-    else:
-      player_one_offset = default_rank
-    offset = random.randint(-pickable_spread,pickable_spread) + player_one_offset
-    if offset < 0:
-      continue
-    if offset >= len(submissions):
-      continue
-    player_two = submissions[offset]
-  log_message("%s (rank %d) vs %s (rank %d) on %s" % (
+  matchup = cloud.get_matchup();
+  player_one = matchup['players'][0]
+  player_two = matchup['players'][1]
+  map_path = "../maps/" + matchup["map"]['name']
+  log_message("%s (rank %s) vs %s (rank %s)" % (
       player_one["submission_id"],
       player_one["rank"],
       player_two["submission_id"],
-      player_two["rank"],
-      map["name"]))
+      player_two["rank"]))
+  cloud.ensure_submission_is_local(player_one["submission_id"],player_one["language_name"],player_one["platform_specific_compilation"])
+  cloud.ensure_submission_is_local(player_two["submission_id"],player_two["language_name"],player_two["platform_specific_compilation"])
+
+  
+  
   # Invoke the game engine.
   player_one_path = "../submissions/" + str(player_one["submission_id"]) + "/."
   player_two_path = "../submissions/" + str(player_two["submission_id"]) + "/."
-  map_path = "../maps/" + map["path"]
   players = [
-    {"path" : player_one_path, "command" : player_one["command"]},
-    {"path" : player_two_path, "command" : player_two["command"]}
+    {"path" : player_one_path, "command" : player_one["command"], "submission_id": player_one["submission_id"]},
+    {"path" : player_two_path, "command" : player_two["command"], "submission_id": player_one["submission_id"]}
   ]
   log_message("starting game")
-  outcome = engine.play_game(map_path, 1000, 200, players, debug=False)
+  try:
+    outcome = engine.play_game(map_path, 1000, 200, players, debug=False)
+  except OSError:
+    print "Game engine threw OS exception"
+    print '-'*60
+    traceback.print_exc(file=sys.stdout)
+    print '-'*60
+    time.sleep(5)
   log_message("game finished")
+  
   # Store the game outcome in the database
   winner = "NULL"
   loser = "NULL"
-  map_id = map["map_id"]
+  map_id = matchup["map"]['id']
   draw = 1
   timestamp = "CURRENT_TIMESTAMP"
   playback_string = ""
@@ -160,31 +153,22 @@ while time.time() - start_time < time_limit:
     playback_string = outcome["playback"]
   if len(errors) == 0:
     log_message("inserting game outcome into the db")
-    cursor.execute("INSERT INTO games (winner,loser,map_id,draw,timestamp," + \
-      "player_one,player_two) VALUES (" + \
-      str(winner) + "," + \
-      str(loser) + "," + \
-      str(map_id) + "," + \
-      str(draw) + "," + \
-      str(timestamp) + "," + \
-      str(player_one["submission_id"]) + "," + \
-      str(player_two["submission_id"]) + ")")
     
-    game_id = connection.insert_id()
-    compressed_playback_string = zlib.compress(playback_string,9)
-    cursor = connection.cursor()
-    cursor.execute(
-        "INSERT INTO playback SET game_id = %s, playback_string = %s",
-        (game_id, compressed_playback_string)
-        )
+    data = {
+      'winner':winner,
+      'loser':loser,
+      'map_id':map_id,
+      'draw':draw,
+      'player_one':player_one['submission_id'],
+      'player_two':player_two['submission_id'],
+      'playback_string':playback_string
+    }
+    
+    cloud.record_game(data)
     
     log_message("finished inserting")
   else:
     log_message(errors)
 
-# Close the database connection
-log_message("closing db connection")
-cursor.close()
-connection.close()
-log_message("db connection closed")
 log_message("exiting")
+os.kill(tm_pid, signal.SIGTERM) # Make sure all threads stop. Is there a better way?
