6dd879f3fdcf7044d2958ea9f90ba6afb46641e9
Daniel Von Fange <daniel@leancoder.com>
2010-09-30 02:11:16 +0000

    Show per server games per minute on games stats page. Fix label on games per minute.
    git-svn-id: https://ai-contest.googlecode.com/svn/trunk@386 9d0e4ec5-6d14-e29b-3149-cabf4038ef1e

--- planet_wars/www/game_stats.php
+++ planet_wars/www/game_stats.php
@@ -7,6 +7,15 @@ foreach(array(5,60,1444) as $minutes){
   $games_per_minute[$minutes] = $r[0];
 }
 
+$games_per_server = array();
+$sql = "select count(*)/5 as gpm, worker from games where timestamp > timestampadd(minute, -5, current_timestamp) group by worker;";
+$q = mysql_query($sql);
+while($r = mysql_fetch_assoc($q)){
+  $games_per_server[] = $r;
+}
+
+
+
 ?>
 
 <h2>Games per minute</h2>
@@ -19,9 +28,25 @@ foreach(array(5,60,1444) as $minutes){
     <td><?=number_format($games_per_minute[1444],1)?></td>
   </tr>
   <tr>
-    <th>Last 15 minutes</th>
+    <th>Last 5 minutes</th>
     <th>Last hour</th>
     <th>Last 24 hours</th>
+  </tr>
+</table>
+
+
+<h2 style="margin-top:1em">Games per minute per server</h2>
+
+<table class="bigstats">
+  <tr>
+  <?php foreach ($games_per_server as $server): ?>
+    <td><?=number_format($server['gpm'])?></td>
+  <?php endforeach ?>
+  </tr>
+  <tr>
+  <?php foreach ($games_per_server as $server): ?>
+    <th>Server #<?=htmlspecialchars($server['worker'])?></th>
+  <?php endforeach ?>
   </tr>
 </table>
 
