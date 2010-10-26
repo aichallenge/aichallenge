aa9b3e4b73e3b5f651a917ff23088f1c7f9256a9
Janzert <janzert@janzert.com>
2010-10-26 19:34:26 +0000

    More semaphore cleanup on the workers
    git-svn-id: https://ai-contest.googlecode.com/svn/branches/20100929-games-in-the-cloud@460 9d0e4ec5-6d14-e29b-3149-cabf4038ef1e

--- planet_wars/backend/tournament_manager_runner.sh
+++ planet_wars/backend/tournament_manager_runner.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd /home/contest/ai-contest/planet_wars/backend/;
-for (( ; ; )); do   sudo -u contest python tournament_manager.py 575 > /dev/null 2> /dev/null; sleep 5; done
+for (( ; ; ));
+do
+    sudo -u contest python tournament_manager.py 575 > /dev/null 2> /dev/null;
+    sleep 5;
+    python semaphore_cleanup.py > /dev/null 2> /dev/null;
+done
