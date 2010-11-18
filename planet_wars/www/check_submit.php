e19a6e16e516608f4ec646679c3b28280e5f8c0f
Benjamin S Wolf <jokeserver@gmail.com>
2010-11-18 06:51:49 +0000

    Merge branches/submit-fix for issue 211.
    git-svn-id: https://ai-contest.googlecode.com/svn/trunk@545 9d0e4ec5-6d14-e29b-3149-cabf4038ef1e

--- planet_wars/www/check_submit.php
+++ planet_wars/www/check_submit.php
@@ -7,15 +7,57 @@ function ends_with($str, $sub) {
   return preg_match('/\Q' . $sub . '\E$/', $str);
 }
 
+function upload_errors($errors) {
+  if (count($_FILES) == 0 || count($_FILES['uploadedfile']) == 0
+      || strlen($_FILES['uploadedfile']['name']) == 0
+      || $_FILES['uploadedfile']['error'] == UPLOAD_ERR_NO_FILE) {
+    $errors[] = "Somehow you forgot to upload a file!";
+  } elseif ($_FILES['uploadedfile']['error'] > 0) {
+    $errors[] = "General upload error: " . $_FILES['uploadedfile']['error'];
+    if ($_FILES['uploadedfile']['error'] == UPLOAD_ERR_FORM_SIZE) {
+      $errors[] = "Your zip file may be larger than the maximum allowed " .
+        "size, 1 MB. You probably have some executables or other larger " .
+        "files in your zip file. Re-zip your submission, being sure to " .
+        "include only the source code.";
+    }
+  } else {
+    $file_size = $_FILES['uploadedfile']['size'];
+    if ($file_size > 2000000) {
+      $errors[] = "File is too big. Maximum size is 1 KB. Make sure that " .
+                  "your zip file contains only your code files.";
+    } else {
+      $filename = basename($_FILES['uploadedfile']['name']);
+      if (!ends_with($filename, ".zip") &&
+          !ends_with($filename, ".tgz") &&
+          !ends_with($filename, ".tar.gz")) {
+        $errors[] = "Invalid file type. Must be zip, tgz, or tar.gz";
+      }
+    }
+  }
+  return $errors;
+}
+
 $submission_directory = "/home/contest/ai-contest/planet_wars/submissions/";
 if (!logged_in_with_valid_credentials()) {
   header('index.php');
 }
-if (has_recent_submission()) {
-  $errors[] = "Sorry your last submission was too recent.";
-} elseif (!setup_submission_directory($submission_directory)) {
-  $errors[] = "Problem while creating submission directory.";
-} else {
+// Uncomment the following line to turn off new submissions.
+//$errors[] = "Nuh-uh. The contest is over. No more submissions.";
+if (count($errors) == 0) {
+  if (has_recent_submission()) {
+    $errors[] = "Sorry your last submission was too recent.";
+  } else {
+    $errors = upload_errors($errors);
+  }
+}
+
+if (count($errors) == 0) {
+  if (!setup_submission_directory($submission_directory)) {
+    $errors[] = "Problem while creating submission directory.";
+  }
+}
+
+if (count($errors) == 0) {
   $destination_folder = $submission_directory . current_submission_id();
   $filename = basename($_FILES['uploadedfile']['name']);
   if (ends_with($filename, ".zip")) { $filename = "entry.zip"; }
@@ -24,47 +66,21 @@ if (has_recent_submission()) {
   $target_path = $destination_folder . '/' . $filename;
   delete_directory($destination_folder);
   mkdir($destination_folder);
-  if ($_FILES['uploadedfile']['error'] > 0) {
-    $errors[] = "General upload error: " . $_FILES['uploadedfile']['error'];
-    if ($_FILES['uploadedfile']['error'] == UPLOAD_ERR_FORM_SIZE) {
-      $errors[] = "Your zip file may be larger than the maximum allowed " .
-        "size, 1 MB. You probably have some executables or other larger " .
-        "files in your zip file. Re-zip your submission, being sure to " .
-        "include only the source code.";
-    }
+  if (!move_uploaded_file($_FILES['uploadedfile']['tmp_name'],
+                          $target_path)) {
+    $errors[] = "Failed to move file from temporary to permanent " .
+                "location.";
     update_current_submission_status(30);
   } else {
-    $file_size = $_FILES['uploadedfile']['size'];
-    if ($file_size > 2000000) {
-      $errors[] = "File is too big. Maximum size is 1 KB. Make sure that " .
-      "your zip file contains only your code files, run.sh, and compile.sh.";
-      update_current_submission_status(30);
-    } else {
-      if (!ends_with($filename, ".zip") &&
-          !ends_with($filename, ".tgz") &&
-          !ends_with($filename, ".tar.gz")) {
-        $errors[] = "Invalid file type. Must be zip, tgz, or tar.gz";
-        update_current_submission_status(30);
-      } else {
-        if (!move_uploaded_file($_FILES['uploadedfile']['tmp_name'],
-                                $target_path)) {
-          $errors[] = "Failed to move file from temporary to permanent " .
-            "location.";
-          update_current_submission_status(30);
-        } else {
-          chmod($destination_folder, 0777);
-          chmod($target_path, 0777);
-          if (!update_current_submission_status(20)) {
-            $errors[] = "Failed to update the submission status in the " .
-              "database.";
-          }
-        }
-      }
+    chmod($destination_folder, 0777);
+    chmod($target_path, 0777);
+    if (!update_current_submission_status(20)) {
+      $errors[] = "Failed to update the submission status in the " .
+                  "database.";
     }
   }
 }
-// Uncomment the following line to turn off new submissions.
-//$errors[] = "Nuh-uh. The contest is over. No more submissions.";
+
 include 'header.php';
 if (count($errors) == 0) {
 ?>
