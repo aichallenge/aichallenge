d07ecb989afed31bdef4d354b0094f864f50569b
Janzert <janzert@janzert.com>
2010-11-26 12:59:22 +0000

    Make readgames use the database much more efficiently
    git-svn-id: https://ai-contest.googlecode.com/svn/trunk@610 9d0e4ec5-6d14-e29b-3149-cabf4038ef1e

--- planet_wars/backend/readgames.py
+++ planet_wars/backend/readgames.py
@@ -14,37 +14,35 @@ connection = MySQLdb.connect(host = server_info["db_host"],
                              db = server_info["db_name"])
 cursor = connection.cursor(MySQLdb.cursors.DictCursor)
 cursor.execute("""
-  SELECT * FROM submissions s JOIN users u ON s.user_id=u.user_id
-  WHERE status = 40 AND latest = 1
-""")
+SELECT g.game_id, g.winner, g.draw,
+    u1.user_id as u1_id, u1.username as u1_name, p1.submission_id as p1_id,
+    u2.user_id as u2_id, u2.username as u2_name, p2.submission_id as p2_id
+  FROM games g LEFT JOIN (submissions p1, submissions p2,
+    users u1, users u2)
+  ON (g.player_one = p1.submission_id AND g.player_two = p2.submission_id
+    AND p1.user_id = u1.user_id AND p2.user_id = u2.user_id)
+  WHERE p1.latest = 1 AND p1.status = 40 AND p2.latest = 1 AND p2.status = 40
+    AND g.timestamp >= NOW() - INTERVAL 10 DAY""")
 
-players = {}
-for row in cursor:
-    s_id = row['submission_id']
-    players[s_id] = "%s,%s,%s" % (row['user_id'], row['username'], s_id)
-
-t = ','.join(str(k) for k in players.keys())
-cursor.execute("""
-  SELECT game_id, winner, loser, map_id, draw, timestamp, player_one, player_two
-  FROM games
-  WHERE player_one IN (%s) AND player_two IN (%s)
-  AND timestamp >= timestampadd(DAY, -10, current_timestamp)""" % (t, t))
 
 pgn_fd, tmp_name = tempfile.mkstemp(dir=".")
 pgn = os.fdopen(pgn_fd, 'w')
 
 for row in cursor:
-    pgn.write('[White "%s"]\n' % (players[row['player_one']],))
-    pgn.write('[Black "%s"]\n' % (players[row['player_two']],))
+    pgn.write('[White "%s,%s,%s"]\n' % (row['u1_id'], row['u1_name'],
+        row['p1_id']))
+    pgn.write('[Black "%s,%s,%s"]\n' % (row['u2_id'], row['u2_name'],
+        row['p2_id']))
     pgn.write('[GameID "%s"]\n' % (row['game_id'],))
     if row['draw']:
         res = '1/2-1/2'
-    elif row['winner'] == row['player_one']:
+    elif row['winner'] == row['p1_id']:
         res = '1-0'
-    elif row['winner'] == row['player_two']:
+    elif row['winner'] == row['p2_id']:
         res = '0-1'
     else:
-        raise ValueError("Winner is not player 1 or 2 game_id "+ row['game_id'])
+        raise ValueError("Winner is not player 1 or 2 game_id "
+                + row['g.game_id'])
     pgn.write('[Result "%s"]\n%s\n\n' % (res, res))
 
 pgn.close()
