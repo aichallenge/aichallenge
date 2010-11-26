15e5f5d3c5a5aea896569ac07c336b705a526b81
Janzert <janzert@janzert.com>
2010-11-26 21:57:59 +0000

    Run readgames directly, so a nice command name is given in process lists
    git-svn-id: https://ai-contest.googlecode.com/svn/trunk@611 9d0e4ec5-6d14-e29b-3149-cabf4038ef1e

--- planet_wars/backend/update_rankings.sh
+++ planet_wars/backend/update_rankings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd `dirname $0`
 start_time="$(date +%s)"
-python readgames.py | ../../third_party/bayeselo/bayeselo > /dev/null 2> /dev/null
+./readgames.py | ../../third_party/bayeselo/bayeselo > /dev/null 2> /dev/null
 #sleep 3
 end_time="$(date +%s)"
 elapsed_seconds="$(expr $end_time - $start_time)"
