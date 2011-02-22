94c45c5f6d8ee92e27a94e34886d7bfb6d8e06e6
McLeopold <mcleopold@gmail.com>
2011-02-21 16:44:15 -0800

    Moved visualizer files to viewer dir.

--- ants/make_movie.cmd
+++ ants/make_movie.cmd
@@ -1,3 +1,3 @@
 @echo off
-playgame.py -r 1 -o visualizer -t %2 -m %1 "python bots/python/HunterBot.py" "python bots/python/LeftyBot.py" "python bots/python/HunterBot.py" "python bots/python/LeftyBot.py"
+playgame.py -r 1 -o viewer -t %2 -m %1 "python bots/python/HunterBot.py" "python bots/python/LeftyBot.py" "python bots/python/HunterBot.py" "python bots/python/LeftyBot.py"
 
