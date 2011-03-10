56f606b750854e560c8d3879ed901fb4123a7c06
McLeopold <mcleopold@gmail.com>
2011-03-10 14:17:39 -0800

    Removed make file and replaced with example scripts using playgame.py.

---
+++ ants/play_one_game.cmd
@@ -0,0 +1,3 @@
+@echo off
+playgame.py -r 1 -j -o viewer -t %2 -m %1 "python bots/python/HunterBot.py" "python bots/python/LeftyBot.py" "python bots/python/HunterBot.py" "python bots/python/LeftyBot.py"
+
---
+++ ants/play_one_game.sh
@@ -0,0 +1,2 @@
+./playgame.py -r 1 -j -o viewer -t %2 -m %1 "python bots/python/HunterBot.py" "python bots/python/LeftyBot.py" "python bots/python/HunterBot.py" "python bots/python/LeftyBot.py"
+
