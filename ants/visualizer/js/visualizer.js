de72397880c93302a1ecc36fe8c4b63b62cbdaf5
Marco Leise <marco.leise@gmx.de>
2011-03-16 17:10:53 +0100

    fixed an invalid character in visualizer.js

--- ants/visualizer/js/visualizer.js
+++ ants/visualizer/js/visualizer.js
@@ -1,6 +1,8 @@
 $import('Ant');
 $import('AppletManager');
-﻿$import('Application');
+$import('Ant');
+$import('AppletManager');
+$import('Application');
 $import('Buttons');
 $import('Config');
 $import('Const');
@@ -15,16 +17,16 @@ var $import_base;
  * @param {String} file the 'package' name
  */
 function $import(file) {
-	var scripts = document.getElementsByTagName("script");
-	if ($import_base === undefined) {
-		var pathLen = scripts[0].src.lastIndexOf('/') + 1;
-		$import_base = scripts[0].src.substr(0, pathLen);
-	}
-	file = $import_base + file.replace(/[.]/g, '/') + '.js'
-	for (var i = 0; i < scripts.length; i++) {
-		if (scripts[i].src === file) {
-			return;
-		}
-	}
-	document.write('<script src="' + file + '"></script>');
+    var scripts = document.getElementsByTagName("script");
+    if ($import_base === undefined) {
+        var pathLen = scripts[0].src.lastIndexOf('/') + 1;
+        $import_base = scripts[0].src.substr(0, pathLen);
+    }
+    file = $import_base + file.replace(/[.]/g, '/') + '.js'
+    for (var i = 0; i < scripts.length; i++) {
+        if (scripts[i].src === file) {
+            return;
+        }
+    }
+    document.write('<script src="' + file + '"></script>');
 }
