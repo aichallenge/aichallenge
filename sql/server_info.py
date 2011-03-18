b5ff755db0f826b0bb09fa4e5a8dcbfbeb34c4f7
McLeopold <mcleopold@gmail.com>
2011-03-17 17:26:10 -0700

    Added script to create test data for matchup query testing

---
+++ sql/server_info.py
@@ -0,0 +1,12 @@
+server_info = {
+    "db_host": "localhost",
+    "db_username": "aichallenge",
+    "db_password": "alert_decay",
+    "db_name": "aichallenge",
+    "mail_name": "test",
+    "mail_username": "",
+    "mail_password": "",
+    "maps_path": "C:/Users/Scott/git/aichallenge/maps/",
+    "submissions_path": "C:/Users/Scott/git/aichallenge/submissions/",
+    "root_path": "C:/Users/Scott/git/aichallenge/"
+}
