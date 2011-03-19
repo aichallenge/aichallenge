1a944f37deb43cf1fb7ce81e2a5f9f344ba7d603
Nick Ham <nick.hammm@gmail.com>
2011-03-20 09:26:12 +1100

    added the random walk symmetric map generator

--- ants/mapgen/symmetric_mapgen.py
+++ ants/mapgen/symmetric_mapgen.py
@@ -5,13 +5,9 @@ import random
 
 #functions	
 def gcd(a, b):
-    minimum = min(a,b)
-    maximum = max(a,b)
-    
-    if minimum == 0:
-        return maximum
-    else:
-        return gcd(minimum, maximum%minimum)
+    while b:
+        a, b = b, a%b
+    return a
         
 def lcm(a, b):
     if a == 0 and b == 0:
@@ -21,31 +17,39 @@ def lcm(a, b):
 
 #map class
 class SymmetricMap():
+    cdirections = ['N', 'E', 'S', 'W']
     directions = {'N': (-1,0), 'S': (1,0), 'E': (0,1), 'W': (0,-1)}
 
     #game parameters
-    min_players = 2
-    max_players = 12
-    min_dim = 35
-    max_dim = 50
-    min_start_distance = 10
-    min_wall_proportion = 0.05
-    max_wall_proportion = 0.2
+    min_players = 3
+    max_players = 10
+    min_dim = 70
+    max_dim = 120
+    min_start_distance = 30
+
+    min_land_proportion = 0.5
+    max_land_proportion = 0.8
+    
+    no_extra_walks = 30
 
     #map parameters
     no_players = 0
     rows = cols = 0
     row_t = col_t = 0
     water_squares = 0
+    land_squares = 0
     map_data = []
-    a_loc = []
+    times_visited = []
+    a_loc = c_locs = []
     
-    #makes a random symmetric map
-    def make_map(self):
+
+    #makes a map by performing a bunch of random walks carving out water
+    def random_walk_map(self):
         self.pick_dimensions()
-        self.map_data = [ ['.' for c in range(self.cols)] for r in range(self.rows) ]
+        self.map_data = [ ['%' for c in range(self.cols)] for r in range(self.rows) ]
         self.add_ants()
-        self.add_water()
+        self.start_walks()
+        self.add_walk_land()
     
     #outputs the map
     def print_map(self):
@@ -62,8 +66,8 @@ class SymmetricMap():
                 self.rows = random.randint(self.min_dim, self.max_dim)
                 self.cols = random.randint(self.min_dim, self.max_dim)
             
-                self.row_t = random.randint(1, self.rows-1)
-                self.col_t = random.randint(1, self.cols-1)
+                self.row_t = random.randint(3, self.rows-3)
+                self.col_t = random.randint(3, self.cols-3)
                 
                 #makes sure no two players start in the same row or column
                 if self.rows/gcd(self.row_t, self.rows) == self.cols/gcd(self.col_t, self.cols):
@@ -87,6 +91,18 @@ class SymmetricMap():
     #randomly picks a location inside the map
     def pick_square(self):
         return [random.randint(0, self.rows-1), random.randint(0, self.cols-1)]
+
+    #starts two random walks from the players starting ants
+    def start_walks(self):
+        self.c_locs = [self.a_loc, self.a_loc]
+        for w in range(self.no_extra_walks):
+            self.c_locs.append(self.pick_square())
+
+    #walks the random walk locations
+    def walk_locations(self):
+        for c in range(len(self.c_locs)):
+            d = self.cdirections[random.randint(0, 3)]
+            self.c_locs[c] = self.get_loc(self.c_locs[c], d)
     
     #returns the new location after moving in a particular direction
     def get_loc(self, loc, direction):                                    
@@ -95,8 +111,8 @@ class SymmetricMap():
         
     #returns the new location after translating it by (rtranslate, ctranslate)
     def get_translate_loc(self, loc):
-        return [(loc[0]+self.row_t+self.rows)%self.rows, 
-                (loc[1]+self.col_t+self.cols)%self.cols ]
+        return [(loc[0]+self.row_t)%self.rows, 
+                (loc[1]+self.col_t)%self.cols ]
     
     #fills in all symmetrically equivalent squares with the given type
     def fill_squares(self, loc, type):
@@ -124,57 +140,42 @@ class SymmetricMap():
         squaresVisited = 1
         
         stack = [start_loc]
-        while stack != []:
+        while stack:
             c_loc = stack.pop()
             for d in self.directions:
                 n_loc = self.get_loc(c_loc, d)
                     
-                if visited[n_loc[0] ][n_loc[1] ] == False and self.map_data[n_loc[0] ][n_loc[1] ] != '%':
+                if not visited[n_loc[0]][n_loc[1]] and self.map_data[n_loc[0]][n_loc[1]] != '%':
                     stack.append(n_loc)
                     visited[n_loc[0] ][n_loc[1] ] = True
                     squaresVisited += 1
-        
-        if squaresVisited + self.water_squares == self.rows*self.cols:
+
+        if squaresVisited == self.land_squares:
             return True
         return False
     
     #adds ants to the map
     def add_ants(self):
+        self.land_squares = self.no_players
         self.a_loc = self.pick_square()
         self.fill_squares(self.a_loc, 'a')
     
-    #adds water to the map
-    def add_water(self):
-        random.normalvariate(2, 5)
-        no_water_squares = random.randint(int(self.min_wall_proportion*self.rows*self.cols), 
-                                          int(self.max_wall_proportion*self.rows*self.cols))
+    #adds land to a map of water
+    def add_walk_land(self):
+        #random.gauss(2,10)
+        no_land_squares = random.randint(int(self.min_land_proportion*self.rows*self.cols), 
+                                          int(self.max_land_proportion*self.rows*self.cols))
+        
+        while self.land_squares < no_land_squares or not self.is_valid():
+            self.walk_locations()
 
-        checked = [ [False for c in range(self.cols)] for r in range(self.rows)]
-        while self.water_squares < no_water_squares:
-            self.water_squares += self.no_players
-
-            #picks out a square that hasn't yet been considered
-            w_square = self.pick_square()
-            while self.map_data[w_square[0] ][w_square[1] ] != '.' or checked[w_square[0] ][w_square[1] ] == True:
-                checked[w_square[0] ][w_square[1] ] = True
-                w_square = self.pick_square()
-            
-            #fills in the selected squares as water
-            self.fill_squares(w_square, '%')
-            checked[w_square[0] ][w_square[1] ] = True
-            
-            #reverts information if it disconnects the map into regions
-            if not self.is_valid():
-                self.fill_squares(w_square, '.')
-                self.water_squares -= self.no_players
-                
-                #marks squares as checked
-                c_loc = w_square
-                for n in range(self.no_players):
-                    checked[c_loc[0] ][c_loc[1] ] = True
-                    c_loc = self.get_translate_loc(c_loc)
+            for c_loc in self.c_locs:
+                if self.map_data[c_loc[0]][c_loc[1]] == '%':
+                    self.land_squares += self.no_players
+                    self.fill_squares(c_loc, '.')
 
 if __name__ == '__main__':
     example_map = SymmetricMap()
-    example_map.make_map()
+    example_map.random_walk_map()
     example_map.print_map()
+
