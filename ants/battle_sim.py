4fbd476d32d6615e087a0f35862889c2c5d637da
sigh <dilshan.a@gmail.com>
2011-03-22 23:26:47 +1100

    Make battle_sim aware that occupied is now power

--- ants/battle_sim.py
+++ ants/battle_sim.py
@@ -102,7 +102,7 @@ if __name__ == "__main__":
 
     print '\n'.join(map_segment)
 
-    for method in ['occupied', 'closest', 'support', 'damage']:
+    for method in ['power', 'closest', 'support', 'damage']:
         result = simulate_battle(map_segment, attackradius2, method)
         result = reset_player_names(map_segment, result)
 
