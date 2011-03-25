f74798a85fbf747b1934cdb9f9c9a2d28eb47b64
McLeopold <mcleopold@gmail.com>
2011-03-25 11:22:05 -0700

    cleaned up and commented python starter bot

--- ants/dist/starter_bots/python/MyBot.py
+++ ants/dist/starter_bots/python/MyBot.py
@@ -1,29 +1,48 @@
 #!/usr/bin/env python
 from ants import *
 
-class TestBot:
+# define a class with a do_turn method
+# the Ants.run method will parse, update and run the method for us
+class MyBot:
+    def __init__(self):
+        # define class level variables, will be remembered between turns
+        pass
+    
+    # do turn is run once per turn
+    # the ants class has the game state and is updated by the Ants.run method
+    # it also has several helper methods to use
     def do_turn(self, ants):
-        destinations = []
-        for a_row, a_col in ants.my_ants():
-            # try all directions in order
+        # loop through all my ants and try to give them orders
+        # the ant_loc is an ant location tuple in (row, col) form
+        for ant_loc in ants.my_ants():
+            # try all directions in given order
             directions = ('n','e','s','w')
             for direction in directions:
-                (n_row, n_col) = ants.destination(a_row, a_col, direction)
-                if (not (n_row, n_col) in destinations and
-                        ants.passable(n_row, n_col)):
-                    ants.issue_order((a_row, a_col, direction))
-                    destinations.append((n_row, n_col))
+                # the destination method will wrap around the map properly
+                # and give us a new (row, col) tuple
+                new_loc = ants.destination(ant_loc, direction)
+                # passable returns true if the location is land
+                if (ants.passable(new_loc)):
+                    # an order is the location of a current ant and a direction
+                    ants.issue_order((ant_loc, direction))
+                    # stop now, don't give 1 ant multiple orders
                     break
-            else:
-                destinations.append((a_row, a_col))
-
+            # check if we still have time left to calculate more orders
+            if ants.time_remaining() < 10:
+                break
+            
 if __name__ == '__main__':
+    # psyco will speed up python a little, but is not needed
     try:
         import psyco
         psyco.full()
     except ImportError:
         pass
+    
     try:
-        Ants.run(TestBot())
+        # if run is passed a class with a do_turn method, it will do the work
+        # this is not needed, in which case you will need to write your own
+        # parsing function and your own game state class
+        Ants.run(MyBot())
     except KeyboardInterrupt:
         print('ctrl-c, leaving ...')
