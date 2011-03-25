190eb25e853f14a727baa001e10c3f4ffbbcde33
sigh <dilshan.a@gmail.com>
2011-03-25 23:54:32 +1100

    Add verbose options to play_one_game.* files since it is now off by default

--- ants/play_one_game.cmd
+++ ants/play_one_game.cmd
@@ -1,3 +1,3 @@
 @echo off
-playgame.py --output_dir viewer --turns 100 --map_file maps/random4.txt "python dist/sample_bots/python/HunterBot.py" "python dist/sample_bots/python/LeftyBot.py" "python dist/sample_bots/python/HunterBot.py" "python dist/sample_bots/python/LeftyBot.py"
+playgame.py --seed 42 --verbose --output_dir viewer --turns 100 --map_file maps/random4.txt "python dist/sample_bots/python/HunterBot.py" "python dist/sample_bots/python/LeftyBot.py" "python dist/sample_bots/python/HunterBot.py" "python dist/sample_bots/python/LeftyBot.py"
 
