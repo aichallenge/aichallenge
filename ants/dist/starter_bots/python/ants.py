7f79955d3a1283ef4e5afd1228124c12dad826c0
McLeopold <mcleopold@gmail.com>
2011-03-26 09:51:31 -0700

    Fixed vision bug to use euclidean instead of manhatten distance

--- ants/dist/starter_bots/python/ants.py
+++ ants/dist/starter_bots/python/ants.py
@@ -197,6 +197,15 @@ class Ants():
 
     def visible(self, loc):
         'determine which squares are visible to the given player'
+    	def vision_distance(self, loc1, loc2):
+	    # this returns the square of the euclidean distance
+            # so it can be compared with the viewradius, which is squared
+	    row1, col1 = loc1
+	    row2, col2 = loc2
+	    d_col = min(abs(col1 - col2), self.width - abs(col1 - col2))
+	    d_row = min(abs(row1 - row2), self.height - abs(row1 - row2))
+	    return d_row**2 + d_col**2
+
         if self.vision == None:
             # cache results for future calls, reset on update
             # start with all spaces not visible
@@ -218,7 +227,7 @@ class Ants():
                     n_loc = self.destination(v_loc, d)
                     n_row, n_col = n_loc
                     if (not self.vision[n_row][n_col] and
-                            self.distance(a_loc, n_loc) <= self.viewradius):
+                            vision_distance(a_loc, n_loc) <= self.viewradius):
                         # we can see this square
                         self.vision[n_row][n_col] = True
                         # add to list to see if other square near it are also
