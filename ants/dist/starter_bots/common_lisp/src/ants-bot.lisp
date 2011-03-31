09c6ac70a4cf953744248a3b1acd38a37c3a2e33
aerique <aerique@xs4all.nl>
2011-03-31 10:32:08 +0200

    More trimming.
    Expand README a little.

--- ants/dist/starter_bots/common_lisp/src/ants-bot.lisp
+++ ants/dist/starter_bots/common_lisp/src/ants-bot.lisp
@@ -29,30 +29,6 @@
                        (turn-time-remaining) " left).~%")))))
 
 
-;; This MAIN is called when you use the Makefile locally.
-(defun main-for-local (&key (log "ants-bot.log") (verbose t))
+;; This MAIN is called when you compile with "make local-bot".
+(defun main-for-local (&key (log "MyBot.log") (verbose t))
   (main :log log :verbose verbose))
-
-
-;; This MAIN is for the Slime REPL with bin/play-proxy-game.sh.
-(defun main-for-proxybot (&key (log "ants-bot-proxied.log") (verbose t)
-                          (host #-allegro #(127 0 0 1) #+allegro "localhost")
-                          (port 41807))
-  (let (client socket stream)
-    (unwind-protect
-         (handler-bind (#+sbcl (sb-bsd-sockets:socket-error
-                                #'socket-error-handler)
-                        (address-in-use-error #'address-in-use))
-           (setf socket (socket-listen host port :reuse-address t))
-           (format *debug-io* "Waiting for connection on ~A:~D...~%"
-                   (host2str host)  port)
-           (force-output)
-           (setf client (socket-accept socket)
-                 stream (socket-stream client))
-           (format *debug-io* "Connected. Playing game...~%")
-           (force-output)
-           (main :state (make-instance 'state :input stream :output stream)
-                 :log log :verbose verbose))
-      (ignore-errors (socket-close client)
-                     (socket-close socket))))
-  (format *debug-io* "Game finished. Connection closed...~%"))
