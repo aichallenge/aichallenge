8aedba2d193e5000d4f0b81edc4ba1e51b42cd16
janzert <janzert@janzert.com>
2011-04-22 17:39:37 -0400

    Add memcached to list of website packages to install

--- setup/server_setup.py
+++ setup/server_setup.py
@@ -23,7 +23,8 @@ def install_manager_packages():
 
 def install_website_packages():
     """ Install system packages required for the website """
-    pkg_list = ["apache2", "php5", "libapache2-mod-php5", "php5-mysql"]
+    pkg_list = ["apache2", "php5", "libapache2-mod-php5", "php5-mysql",
+            "memcached"]
     install_apt_packages(pkg_list)
 
 def setup_base_files(opts):
