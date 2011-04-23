4e01c92f376c6b49d812e0369ff52d8f27e2a6d0
Alexandru Stan <alex@hypertriangle.com>
2011-04-22 22:08:43 -0400

    perfected the setup.py file, linked index.php

--- website/setup.py
+++ website/setup.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
+import glob
+import re
 import os
 
-prefix, competition, suffix = "aichallenge.wiki/", "Ants", ".md"
-mapping = {
-    "index.php": "%s",
-}
-toreplace = "ContentFromTheWiki"
+competition="Ants"
+seewikiregex=re.compile(r'(<MarkdownReplacement with="([^\n>]*.md)" />)',re.DOTALL)
+markdownlocation = "aichallenge.wiki/"
 
-for page in mapping.keys():
-    mapping[page] = (prefix+mapping[page]+suffix) % (competition,)
-
-if __name__=="__main__":
-    for page in mapping.keys():
-        pagecontent=open(page).read()
-        compiledmarkdown=os.popen("markdown %s" % (mapping[page])).read()
+for page in glob.iglob("*.php"):
+    pagecontent=open(page).read()
+    
+    matches=(match.groups() for match in seewikiregex.finditer(pagecontent))
+    for toreplace, markdownfilename in matches:
+        markdownfilename=markdownfilename.replace("competition",competition)
+        print "Inserting `%s` into `%s`, where `%s` was." % (markdownfilename,page,toreplace)
+        compiledmarkdown=os.popen("markdown %s" % markdownlocation+markdownfilename).read()
+        compiledmarkdown='<!--<MarkdownReplacement from="%s">-->%s<!--</MarkdownReplacement>-->' % (markdownfilename,compiledmarkdown)
         pagecontent=pagecontent.replace(toreplace,compiledmarkdown)
-        print pagecontent
-        
+    open(page).write(pagecontent)
+    
