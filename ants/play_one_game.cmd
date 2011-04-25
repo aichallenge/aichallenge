a316d79c7e713ccc39cfa9eb7eff20bdf065b007
sigh <dilshan.a@gmail.com>
2011-04-25 14:41:23 +1000

    Make --log_stderr the default

--- ants/play_one_game.cmd
+++ ants/play_one_game.cmd
@@ -1,3 +1,3 @@
 @echo off
-playgame.py --seed 42 --verbose --output_dir viewer --turns 100 --map_file maps/symmetric_maps/symmetric_10.map "python dist/sample_bots/python/HunterBot.py" "python dist/sample_bots/python/LeftyBot.py" "python dist/sample_bots/python/HunterBot.py" "python dist/sample_bots/python/LeftyBot.py"
+playgame.py --seed 42 --verbose --output_dir viewer --log_stderr --turns 100 --map_file maps/symmetric_maps/symmetric_10.map "python dist/sample_bots/python/HunterBot.py" "python dist/sample_bots/python/LeftyBot.py" "python dist/sample_bots/python/HunterBot.py" "python dist/sample_bots/python/LeftyBot.py"
 
