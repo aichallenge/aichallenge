9ce3944d120934524c4f3a5d98d13ec78caa7621
McLeopold <mcleopold@gmail.com>
2011-05-11 20:52:52 -0700

    made add maps more friendly

--- manager/add_maps_to_database.py
+++ manager/add_maps_to_database.py
@@ -9,11 +9,12 @@ from sql import sql
 
 def main():
     # get list of all map files
+    maps_path = server_info["maps_path"]
     map_files = set()
-    for root, dirs, files in os.walk(server_info["maps_path"]):
+    for root, dirs, files in os.walk(maps_path):
         for filepath in files:
             if filepath.endswith(".map"):
-                map_files.add(os.path.join(root, filepath))
+                map_files.add(os.path.join(root, filepath)[len(maps_path)+1:])
     
     # get list of maps in database
     connection = MySQLdb.connect(host = server_info["db_host"],
@@ -28,17 +29,22 @@ def main():
     new_maps = map_files.difference(db_maps)
     
     # add new maps to database with top priority
-    cursor.execute(sql["update_map_priorities"])
-    for mapfile in new_maps:
-        players = 0
-        with open(mapfile, 'r') as f:
-            for line in f:
-                if line.startswith('players'):
-                    players = int(line.split()[1])
-                    break
-        if players:
-            cursor.execute(sql["insert_map_filenames"], (mapfile, players))
-    connection.commit()
+    if len(new_maps) > 0:
+        cursor.execute(sql["update_map_priorities"])
+        for mapfile in new_maps:
+            players = 0
+            with open(os.path.join(maps_path,mapfile), 'r') as f:
+                for line in f:
+                    if line.startswith('players'):
+                        players = int(line.split()[1])
+                        break
+            if players:
+                cursor.execute(sql["insert_map_filenames"], (mapfile, players))
+            print(mapfile)
+        connection.commit()
+        print('{0} maps added to database'.format(len(new_maps)))
+    else:
+        print("No maps added, priorities not changed.")
     
 if __name__ == "__main__":
     main()
