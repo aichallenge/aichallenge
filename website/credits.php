985a59ee8f41dd3fbdf489be00840002336ea996
Alexandru Stan <alex@hypertriangle.com>
2011-05-22 17:37:38 -0400

    redesigned the credits page

--- website/credits.php
+++ website/credits.php
@@ -1,36 +1,41 @@
 <?php include 'header.php'; ?>
 
-<h2>Special Thanks</h2>
-<p>The Google AI Challenge is designed, built, and operated completely by
-  volunteers. As the number of participants has steadily grown, so has the
-  amount of work required to make it all happen.</p>
-<p>With the move to
-  <a href="https://github.com/aichallenge/aichallenge">fully open-source</a>,
-  many of the contestants have been contributing code to help us
-  keep up with the volume of feature requests and bug fixes. <!--All-in-all,
-  several dozen people have contributed to the design, implementation,
-  and operation of the Fall 2010 Google AI Challenge. To all the hackers
-  who sent us patches to fix bugs, created new and interesting visualizers,
-  set up independent game servers, translated the starter packages into new
-  languages, and told us how to fix the webserver whenever it got crushed by
-  traffic spikes from Reddit, we are truly grateful.--> Thank you.</p>
-<p>There are a few people who have been especially generous with their time
-  and expertise. Our special thanks goes out to these individuals.</p>
-<h3>Janzert</h3>
-<p>Janzert was originally a contestant. He joined the core admin team so that
-  he could work on improving and stabilizing the contest systems. <!--From October
-  onwards, he was the primary operator of the entire contest backend,
-  including the newly created cluster of game-playing servers. He boosted
-  the throughput of the system from an average of 8 games per minute to
-  our current level of 134, meaning that each submission gets to play a lot
-  more games.--></p>
-<p><!--Janzert contributed dozens of new features and bug fixes, eliminating
-  90% of the problems that people found most frustrating about the contest.
-  He is a machine.--></p>
-<h3>Alex Stan</h3>
-<p>Better known as amstan, Alex was in charge of the forums and the IRC
-  channel. He worked tirelessly to make sure that people's questions got
-  answered, and kept the development team focused on the things that
-  mattered most to the contest participants.</p>
-<p>Alex was the winner of the first Google AI Challenge.</p>
+<!--Note to commiters: Feel free to add yourself in here.-->
+
+<h2>Credits</h2>
+<p>The Google AI Challenge is designed, built, and operated completely by volunteers. As the number of participants has steadily grown, so has the amount of work required to make it all happen.</p>
+<p>With the move to <a href="https://github.com/aichallenge/aichallenge">fully open-source</a>, many of the contestants have been contributing code to help us keep up with the volume of feature requests and bug fixes. <!--All-in-all, several dozen people have contributed to the design, implementation, and operation of the Fall 2010 Google AI Challenge. To all the hackers who sent us patches to fix bugs, created new and interesting visualizers, set up independent game servers, translated the starter packages into new languages, and told us how to fix the webserver whenever it got crushed by traffic spikes from Reddit, we are truly grateful.--> Thank you.</p>
+<p>There are a few people who have been especially generous with their time and expertise. Our special thanks goes out to these individuals.</p>
+
+<h3>Ants</h3>
+<ul>
+	<li><strong>Original Idea:</strong> Mistmanov</li>
+	<li>j3camero (Jeff Cameron)</li>
+	<li>antimatroid (Nick Ham)</li>
+	<li>McLeopold (Scott Hamilton)</li>
+	<li><strong>Visualizer:</strong> Frontier (Marco Leise)</li>
+</ul>
+
+<h3>Website</h3>
+<ul>
+	<li><strong>Design:</strong> computerwiz_222 (Andrew Rossignol)</li>
+	<li>amstan (Alexandru Stan)</li>
+	<li>McLeopold (Scott Hamilton)</li>
+</ul>
+
+<h3>Manager &amp; Worker</h3>
+<ul>
+	<li>Janzert</li>
+	<li>McLeopold (Scott Hamilton)</li>
+	<li>sigh</li>
+</ul>
+
+<h3>Special Thanks</h3>
+<ul>
+	<li><a href="https://github.com/aichallenge/aichallenge/tree/epsilon/ants/dist/starter_bots">Starter Package Makers</a></li>
+	<li><strong>Beta Server:</strong> demerzel</li>
+</ul>
+
+<p><a href="https://github.com/aichallenge/aichallenge/contributors">Full list of commiters</a></p>
+
 <?php include 'footer.php'; ?>
