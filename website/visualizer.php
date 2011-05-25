7455a1173695cd69f58a832ef3af8fad75ca740d
McLeopold <mcleopold@gmail.com>
2011-05-25 15:55:07 -0700

    Fixed XSS issue.  Closes #124

--- website/visualizer.php
+++ website/visualizer.php
@@ -1,6 +1,11 @@
 <?php include "header.php"; ?>
 <?php
     include "visualizer_widget.php";
-    visualizer_widget($_GET["game"]);
+    $game_id = filter_input(INPUT_GET, 'game', FILTER_VALIDATE_INT);
+    if ($game_id) {
+        visualizer_widget($game_id);
+    } else {
+        echo '<p>Incorrect Game Number</p>';
+    }
 ?>
 <?php include "footer.php"; ?>
