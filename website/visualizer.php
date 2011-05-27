187a25864d89a556f4e534a38e5fc70d360e5b2c
McLeopold <mcleopold@gmail.com>
2011-05-26 23:10:12 -0700

    fixed no game bug

--- website/visualizer.php
+++ website/visualizer.php
@@ -2,7 +2,7 @@
 <?php
     include "visualizer_widget.php";
     $game_id = filter_input(INPUT_GET, 'game', FILTER_VALIDATE_INT);
-    if ($game_id !== FALSE) {
+    if ($game_id !== FALSE and $game_id !== NULL) {
         visualizer_widget($game_id);
     } else {
         echo '<p>Incorrect Game Number</p>';
