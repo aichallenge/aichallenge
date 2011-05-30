c7d21fd325fec4140da8eb5e9cd8457a02cebaee
McLeopold <mcleopold@gmail.com>
2011-05-29 21:36:31 -0700

    attempt to show game errors

--- website/sql.php
+++ website/sql.php
@@ -241,7 +241,16 @@ $sql = array(
          on g.game_id = gp.game_id
      inner join user u
          on gp.user_id = u.user_id
-     order by g.game_id desc, gp.game_rank"
+     order by g.game_id desc, gp.game_rank",
+    "select_game_errors" => "select gp.user_id, gp.errors, gp.status, u.username
+        from game_player gp,
+        inner join user u
+            on u.user_id = gp.user_id
+        where game_id = %s
+        and (gp.status = 'timeout'
+            or gp.status = 'crashed'
+            or gp.status = 'invalid')
+    ";
 );
 
 ?>
--- website/visualizer.php
+++ website/visualizer.php
@@ -1,11 +1,38 @@
-<?php include "header.php"; ?>
 <?php
-    include "visualizer_widget.php";
-    $game_id = filter_input(INPUT_GET, 'game', FILTER_VALIDATE_INT);
-    if ($game_id !== FALSE and $game_id !== NULL) {
-        visualizer_widget($game_id);
-    } else {
-        echo '<p>Incorrect Game Number</p>';
+
+include "header.php";
+
+include "visualizer_widget.php";
+$game_id = filter_input(INPUT_GET, 'game', FILTER_VALIDATE_INT);
+if ($game_id !== FALSE and $game_id !== NULL) {
+    visualizer_widget($game_id);
+    require_once('session.php');
+    if (logged_in_with_valid_credentials()) {
+        require_once('mysql_login.php');
+        $game_errors = contest_query('select_game_errors',
+                                     $game_id);
+        if ($game_errors) {
+        	$error_msg = "<ul>";
+        	while ($row = mysql_fetch_assoc($game_errors)) {
+        		// TODO: turn off for all users for contest
+        		// also make the query user specific
+        		if (TRUE or $row["user_id"] == current_user_id()) {
+        			$username = $row["username"];
+        			$status = $row["status"];
+        			$error_msg .= "<li><p>$username - $status</p><pre class=\"error\">";
+                    $error_msg .= str_replace('\n', "\n", $row["errors"])."\n";
+        			$error_msg .= "</pre></li>";
+        		}
+        	}
+        	$error_msg .= "</ul>";
+        	echo $error_msg;
+        }	
     }
+    
+} else {
+    echo '<p>Incorrect Game Number</p>';
+}
+
+include "footer.php";
+
 ?>
-<?php include "footer.php"; ?>
