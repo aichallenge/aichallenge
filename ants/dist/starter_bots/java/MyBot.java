b8728f8ce5d4a61325aeb9fb41e5782bc4908fec
unknown <wroe1587@.nsn-intra.net>
2011-06-11 20:41:54 +0200

    removed r lines support

--- ants/dist/starter_bots/java/MyBot.java
+++ ants/dist/starter_bots/java/MyBot.java
@@ -4,30 +4,31 @@ import java.io.IOException;
  * Starter bot implementation.
  */
 public class MyBot extends Bot {
-	/**
-	 * Main method executed by the game engine for starting the bot.
-	 * 
-	 * @param args command line arguments
-	 * 
-	 * @throws IOException if an I/O error occurs
-	 */
-	public static void main(String[] args) throws IOException {
-		new MyBot().readSystemInput();
-	}
+    /**
+     * Main method executed by the game engine for starting the bot.
+     * 
+     * @param args command line arguments
+     * 
+     * @throws IOException if an I/O error occurs
+     */
+    public static void main(String[] args) throws IOException {
+        new MyBot().readSystemInput();
+    }
     
     /**
      * For every ant check every direction in fixed order (N, E, S, W) and move it if the tile is
      * passable.
      */
-	@Override
+    @Override
     public void doTurn() {
-	    Ants ants = getAnts();
-	    for (Tile myAnt : ants.getMyAnts()) {
-	        for (Aim direction : Aim.values()) {
-	            if (ants.getIlk(myAnt, direction).isPassable()) {
-	                ants.issueOrder(myAnt, direction);
-	            }
-	        }
-	    }
+        Ants ants = getAnts();
+        for (Tile myAnt : ants.getMyAnts()) {
+            for (Aim direction : Aim.values()) {
+                if (ants.getIlk(myAnt, direction).isPassable()) {
+                    ants.issueOrder(myAnt, direction);
+                    break;
+                }
+            }
+        }
     }
 }
