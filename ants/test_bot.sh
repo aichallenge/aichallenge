7bdbcc07dd9343b3e9ab0b9fa1b3dad429b000c8
aerique <aerique@xs4all.nl>
2011-06-14 15:39:26 +0200

    Replace --seed with --engine_seed and --player_seed.

--- ants/test_bot.sh
+++ ants/test_bot.sh
@@ -1,1 +1,1 @@
-./playgame.py --seed 42 --food none --end_wait=0.25 --verbose --log_dir game_logs --turns 30 --map_file submission_test/test.map "$1" "python submission_test/TestBot.py" --nolaunch -e --strict --capture_errors
+./playgame.py --engine_seed 42 --player_seed 42 --food none --end_wait=0.25 --verbose --log_dir game_logs --turns 30 --map_file submission_test/test.map "$1" "python submission_test/TestBot.py" --nolaunch -e --strict --capture_errors
