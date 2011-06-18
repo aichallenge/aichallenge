55dfe4658bd16cc32fa813583aa5c0c0d32e58f0
janzert <janzert@janzert.com>
2011-06-18 01:22:36 -0400

    Move blockade testing script to util directory

---
+++ ants/util/block_test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd ..
+./playgame.py -m maps/test_maps/column_3.map "python dist/sample_bots/python/HoldBot.py" "python dist/starter_bots/python/MyBot.py" --engine_seed 1 --player_seed 1 --verbose -t 50 -l game_logs --attack power --food none --attackradius2 5
+
