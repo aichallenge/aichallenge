1416f23735818b6114191e6e1739a3fd28b5c989
McLeopold <mcleopold@gmail.com>
2011-06-29 15:25:19 -0700

    update to leaderboard generation, ghosts drop off after new submissions sigma is better or is 48 old or is worse than current, change values calculated from last game, not last leaderboard

--- sql/2_generate_leaderboard.sql
+++ sql/2_generate_leaderboard.sql
@@ -18,32 +18,42 @@ select
     s.user_id, s.submission_id, s.version,
     @count1 := @count1 + 1 as seq,
     @rank := if(s.latest > 0, @count2 := @count2 + 1 , null) as rank,
-    r.rank - @rank as rank_change,
-    s.mu as mu, s.mu - r.mu as mu_change,
-    s.sigma as sigma, s.sigma - r.sigma as sigma_change,
-    @skill := if(3*s.sigma>s.mu, 0.0, s.mu - s.sigma * 3) as skill, @skill - r.skill as skill_change,
+    gp.rank - @rank as rank_change,
+    s.mu as mu, gp.mu_after - gp.mu_before as mu_change,    
+    s.sigma as sigma, gp.sigma_after - gp.sigma_before as sigma_change,
+    @skill := if(3*s.sigma>s.mu, 0.0, s.mu - s.sigma * 3) as skill,
+    (gp.mu_after - gp.sigma_after * 3) - (gp.mu_before - gp.sigma_before * 3) as skill_change,
     s.latest, timediff(now(),s.timestamp) as age
 from
     (select * from submission order by mu - sigma * 3 desc, mu desc, sigma asc, submission_id asc) s
     -- inner join to ensure both user and submission record exists
     inner join user u
         on s.user_id = u.user_id
-    left outer join ranking r
-        on s.submission_id = r.submission_id and r.leaderboard_id = @last_leader,
+    -- these 2 joins bring in the last game_player for a submission
+    left outer join (select submission_id, max(game_id) as game_id
+                     from game_player
+                     group by submission_id) gpmax
+        on gpmax.submission_id = s.submission_id
+    left outer join game_player gp
+        on gp.submission_id = gpmax.submission_id
+        and gp.game_id = gpmax.game_id,    
     (select @count1 := 0) c1,
     (select @count2 := 0) c2
 where s.latest = 1
-or s.submission_id in (
-    select submission_id
-    from ranking
-    where leaderboard_id = @last_leader
-    and seq in (
-        select min(seq)
-        from ranking
-        where leaderboard_id = @last_leader
-        group by user_id
-    )
-);
+-- ghost requirements
+or (
+    -- can only be 48 hours old
+    s.timestamp > timestampadd(hour, -400, current_timestamp)
+    -- must have smaller sigma (more confident than current)
+    and s.sigma = (select min(sigma)
+                   from submission s2
+                   where s2.user_id = s.user_id)
+    -- must be a higher rank
+    and (s.mu - s.sigma * 3) > (select mu - sigma * 3
+                                from submission s3
+                                where s3.user_id = s.user_id
+                                and latest = 1)
+    );
 
 -- sigma will be updated on a per game basis
 -- update submission
