764ef74064325635eed90e59e3ebdb16fbbcbcc5
McLeopold <mcleopold@gmail.com>
2011-09-09 17:17:18 -0700

    fix map.php link, closes #249

--- website/map.php
+++ website/map.php
@@ -5,7 +5,7 @@ include "header.php";
 include "visualizer_widget.php";
 $map = str_replace("%2F", "/", filter_input(INPUT_GET, 'map', FILTER_SANITIZE_ENCODED));
 if ($map !== FALSE and $map !== NULL) {
-    echo $map;
+    echo "<a href=\"map/$map\">$map</a> (download link)";
     visualize_map($map);    
 } else {
     echo "<p>Incorrect Map $map</p>";
