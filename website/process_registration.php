077050d22ecde1113d24c9d1da69966d446e3f39
janzert <janzert@janzert.com>
2011-09-12 10:18:41 -0400

    Check email for valid domain before accepting registration.

--- website/process_registration.php
+++ website/process_registration.php
@@ -91,7 +91,11 @@ if (strcmp($user_email, "donotsend") != 0) {
   $sql="select email from user where email = '$user_email' and admin = 0";
   $result = mysql_query($sql);
   if ($result && mysql_num_rows($result) > 0) {
-    $errors[] = "<p>The email $user_email is already in use. You are only allowed to have one account! It is easy for us to tell if you have two accounts, and you will be disqualified if you have two accounts! If there is some problem with your existing account, get in touch with the contest organizers on irc.freenode.com channel #aichallenge and we will help you get up-and-running again!</p>";
+    $errors[] = "The email $user_email is already in use. You are only allowed to have one account! It is easy for us to tell if you have two accounts, and you will be disqualified if you have two accounts! If there is some problem with your existing account, get in touch with the contest organizers on irc.freenode.com channel #aichallenge and we will help you get up-and-running again!";
+  }
+  $edomain = substr(strrchr($user_email, '@'), 1);
+  if (strcmp(gethostbyname($edomain), $edomain) == 0) {
+    $errors[] = "Could not find the email address entered. Please enter a valid email address.";
   }
 }
 
