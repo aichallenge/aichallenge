f72dc3a9bf4ea8aef5dde7975b66824499654cd0
McLeopold <mcleopold@gmail.com>
2011-09-15 23:09:56 -0700

    fix json reference

--- website/api_game_result.php
+++ website/api_game_result.php
@@ -77,7 +77,7 @@ if (array_key_exists('error', $gamedata)) {
 } else {
     // move matchup data to game table
     if (!contest_query("insert_game_data",
-                       $gamedata->turns,
+                       $gamedata->replaydata->turns,
                        $gamedata->game_length,
                        $gamedata->replaydata->winning_turn,
                        $gamedata->replaydata->ranking_turn,
