d4691de348f3ac60dde38ff0059c307e9a02a3f3
McLeopold <mcleopold@gmail.com>
2011-09-21 16:15:33 -0700

    website improvements

--- website/visualizer.php
+++ website/visualizer.php
@@ -4,20 +4,24 @@ include "header.php";
 
 include "visualizer_widget.php";
 $game_id = filter_input(INPUT_GET, 'game', FILTER_VALIDATE_INT);
+$user_id = filter_input(INPUT_GET, 'user', FILTER_VALIDATE_INT);
 if ($game_id !== FALSE and $game_id !== NULL) {
     visualize_game($game_id);
     require_once('session.php');
-    if (TRUE or logged_in_with_valid_credentials()) {
+    if ($user_id &&
+            logged_in_with_valid_credentials() &&
+            current_user_id() === $user_id) {
         require_once('mysql_login.php');
         $game_errors = contest_query('select_game_errors',
-                                     $game_id);
+                                     $game_id,
+                                     $user_id);
         if ($game_errors) {
         	$error_msg = "<ul>";
         	while ($row = mysql_fetch_assoc($game_errors)) {
         		// TODO: turn off for all users for contest
         		// also make the query user specific
         		if (TRUE or $row["user_id"] == current_user_id()) {
-        			$username = $row["username"];
+        			$username = current_username();
         			$status = $row["status"];
         			$error_msg .= "<li><p>$username - $status</p><pre class=\"error\">";
                     $error_msg .= str_replace('\n', "\n", $row["errors"])."\n";
