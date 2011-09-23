1c27ea2f02b36389c68c6bf7448925a92a2e988f
janzert <janzert@janzert.com>
2011-09-23 16:03:34 -0400

    Use Erlang convention for bot name

--- worker/compiler.py
+++ worker/compiler.py
@@ -387,8 +387,8 @@ languages = (
         [],
         [(["*.ss"], ChmodCompiler("Scheme"))]
     ),
-    Language("Erlang", ".beam", "MyBot.erl",
-        "erl -hms"+ str(MEMORY_LIMIT) +"m -smp disable -noshell -s MyBot start -s init stop",
+    Language("Erlang", ".beam", "my_bot.erl",
+        "erl -hms"+ str(MEMORY_LIMIT) +"m -smp disable -noshell -s my_bot start -s init stop",
         ["*.beam"],
         [(["*.erl"], TargetCompiler(comp_args["Erlang"][0], targets["Erlang"]))]
     ),
