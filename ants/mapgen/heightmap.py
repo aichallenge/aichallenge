5aa1370b0268d27aab0ab09d1a8dd937d6d6e519
janzert <janzert@janzert.com>
2011-09-26 16:28:23 -0400

    Cleanup extraneous whitespace in map generators

--- ants/mapgen/heightmap.py
+++ ants/mapgen/heightmap.py
@@ -11,13 +11,13 @@ class HeightMapMap(Map):
         self.cols = options.get('cols', (40,120))
         self.players = options.get('players', (2,4))
         self.land = options.get('land', (85, 90))
-    
+
     def generate_heights(self, size):
         rows, cols = size
-        
+
         # initialize height map
         height_map = [[0]*cols for _ in range(rows)]
-    
+
         # cut and lift
         iterations = 1000
         for _ in range(iterations):
@@ -39,8 +39,8 @@ class HeightMapMap(Map):
         min_height = min([min(hrow) for hrow in hmap])
         for row in range(rows):
             for col in range(cols):
-                hmap[row][col] -= min_height                
-    
+                hmap[row][col] -= min_height
+
     def local_min(self, hmap):
         rows = len(hmap)
         cols = len(hmap[0])
@@ -54,17 +54,17 @@ class HeightMapMap(Map):
                         break
                 else:
                     min_list.append((row, col))
-        return min_list        
-    
+        return min_list
+
     def generate_rivers(self, hmap):
         rows = len(hmap)
         cols = len(hmap[0])
-        
+
         # new height map
         min_list = self.local_min(hmap)
         self.normalize(hmap)
         water_map = [[0] * cols for _ in range(rows)]
-        
+
         # place drop of water, follow to lowest point
         for w_row in range(rows):
             for w_col in range(cols):
@@ -77,7 +77,7 @@ class HeightMapMap(Map):
                     for d_row, d_col in ((1,0), (0,1), (-1,0), (0,-1)):
                         h_row = (c_row + d_row) % rows
                         h_col = (c_col + d_col) % cols
-                        if not (h_row, h_col) in water_path: 
+                        if not (h_row, h_col) in water_path:
                             h[hmap[h_row][h_col]] += [(h_row, h_col)]
                     # select randomly if there are 2 squares at the same height
                     if len(h) == 0:
@@ -87,60 +87,60 @@ class HeightMapMap(Map):
                         min_height = min(h.keys())
                         if min_height >= hmap[c_row][c_col]:
                             # point lower, move
-                            c_row, c_col = choice(h[min_height])                                
+                            c_row, c_col = choice(h[min_height])
                         else:
                             # no point lower
                             break
         return water_map
-    
+
     def generate(self):
         # pick random full size for map
-        rows = self.get_random_option(self.rows)        
+        rows = self.get_random_option(self.rows)
         cols = self.get_random_option(self.cols)
-        
+
         # ensure map rows:cols ratio is within 2:1
         while cols < rows // 2 or cols > rows * 2:
             cols = self.get_random_option(self.cols)
-            
+
         # calc max players that can be tiled
         row_max = rows//16
         col_max = cols//16
         player_max = row_max*col_max
-        
+
         players = self.get_random_option(self.players)
         # ensure player count choosen is within max
         while players > player_max:
             players = self.get_random_option(self.players)
-            
+
         # pick random grid size
         # ensure grid rows < row_max
         # ensure grid cols < col_max
         divs = [(i, players//i)
                 for i in range(1,min(players+1, row_max+1))
-                if players % i == 0        
+                if players % i == 0
                     and players//i < col_max]
         if len(divs) == 0:
             # there were no acceptable grid sizes for map
             # usually do to a prime number of players which has to be 1xN
             return self.generate()
         row_sym, col_sym = choice(divs)
-        
+
         # fix dimensions for even tiling
         rows //= row_sym
-        cols //= col_sym            
-        
+        cols //= col_sym
+
         # get percent of map that should be land
-        land = self.get_random_option(self.land)        
+        land = self.get_random_option(self.land)
 
         height_map = self.generate_heights((rows, cols))
         height_map = self.generate_rivers(height_map)
-        
+
         # create histogram
         histo = defaultdict(int)
         for height_row in height_map:
             for height in height_row:
                 histo[height] += 1
-    
+
         # find sea and snow levels
         map_area = rows * cols
         sea_level = min(histo.keys())
@@ -158,7 +158,7 @@ class HeightMapMap(Map):
 
         # initialize map
         self.map = [[LAND]*cols for _ in range(rows)]
-                
+
         # place salty and frozen water
         for row in range(rows):
             for col in range(cols):
@@ -167,14 +167,14 @@ class HeightMapMap(Map):
                     self.map[row][col] = WATER
         #self.toText()
         self.fill_small_areas()
-        
+
         # check too make sure too much wasn't filled in, only 2 percent of area
         areas = self.section(0)
         water_area = map_area - len(areas[0][0])
         added_area = water_area - snow_area - sea_area
         if map_area * 2 // 100 < added_area:
             return self.generate()
-        
+
         # place player start in largest unblockable section
         areas = self.section()
         row, col = choice(areas[0][0])
@@ -184,19 +184,19 @@ class HeightMapMap(Map):
         d_col = cols//2 - col
         self.translate((d_row, d_col))
 
-        # finish map        
+        # finish map
         #self.tile((row_sym, col_sym))
         self.make_wider()
-        
+
 def main():
     new_map = HeightMapMap()
     new_map.generate()
-    
+
     # check that all land area is accessable
     #while new_map.allowable() != None:
         #new_map.generate()
-        
+
     new_map.toText()
-    
+
 if __name__ == '__main__':
     main()
--- ants/mapgen/random_map.py
+++ ants/mapgen/random_map.py
@@ -9,7 +9,7 @@ class RandomMap(Map):
         self.cols = options.get('cols', (40,120))
         self.players = options.get('players', (2,26))
         self.land = options.get('land', (80, 95))
-    
+
     def generate(self):
         rows = self.get_random_option(self.rows)
         cols = self.get_random_option(self.cols)
@@ -18,7 +18,7 @@ class RandomMap(Map):
 
         # initialize map
         self.map = [[LAND]*cols for _ in range(rows)]
-        
+
         # place water
         water = rows*cols*(100-land)//100
         row = 0
@@ -28,18 +28,18 @@ class RandomMap(Map):
                 row = randint(0, rows-1)
                 col = randint(0, cols-1)
             self.map[row][col] = WATER
-        
+
         # place player starts
         for player in range(players):
             while self.map[row][col] != LAND:
                 row = randint(0, rows-1)
                 col = randint(0, cols-1)
-            self.map[row][col] = player        
-        
+            self.map[row][col] = player
+
 def main():
     new_map = RandomMap()
     new_map.generate()
     new_map.toText()
-    
+
 if __name__ == '__main__':
     main()
