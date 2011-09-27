50f49f261ba6e610d3c96ddba863cf4dcce85ff3
janzert <janzert@janzert.com>
2011-09-26 21:09:43 -0400

    Quick page to list server games

---
+++ website/games.php
@@ -0,0 +1,11 @@
+<?php
+require_once("header.php");
+require_once("game_list.php");
+
+echo "<h2>All games by time played</h2>";
+
+$page = get_type_or_else("page", FILTER_VALIDATE_INT, 1);
+echo get_game_list_table($page);
+
+require_once("footer.php");
+?>
