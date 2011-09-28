060e166fd8d0197c6c24e717aefcadac78732a57
McLeopold <mcleopold@gmail.com>
2011-09-28 01:22:00 -0700

    Update specification

--- website/specification.php
+++ website/specification.php
@@ -6,6 +6,59 @@ visualize_pre();
 
 ?>
 
+<ol>
+<li><a href="#Turns-and-Phases">Turns and Phases</a></li>
+<li><a href="#Scoring">Scoring</a></li>
+<li><a href="#Cutoff-Rules">Cutoff Rules</a></li>
+<li><a href="#Ant-Spawning">Ant Spawning</a></li>
+<li><a href="#Food-Spawning">Food Spawning</a></li>
+<li><a href="#Battle-Resolution">Battle Resolution</a></li>
+<li><a href="#Bot-Input">Bot Input</a></li>
+<li><a href="#Bot-Output">Bot Output</a></li>
+<li><a href="#Map-Format">Map Format</a></li>
+<li><a href="#Replay-Format">Replay Format</a></li>
+</ol>
+
+<div id="Turns-and-Phases">
+<!--<MarkdownReplacement with="competition-Turns-and-Phases.md">--><!--</MarkdownReplacement>-->
+</div>
+
+<div id="Scoring">
+<!--<MarkdownReplacement with="competition-Scoring.md">--><!--</MarkdownReplacement>-->
+</div>
+
+<div id="Cutoff-Rules">
+<!--<MarkdownReplacement with="competition-Cutoff-Rules.md">--><!--</MarkdownReplacement>-->
+</div>
+
+<div id="Ant-Spawning">
+<!--<MarkdownReplacement with="competition-Ant-Spawning.md">--><!--</MarkdownReplacement>-->
+</div>
+
+<div id="Food-Spawning">
+<!--<MarkdownReplacement with="competition-Food-Spawning.md">--><!--</MarkdownReplacement>-->
+</div>
+
+<div id="Battle-Resolution">
+<!--<MarkdownReplacement with="competition-Battle-Resolution".md">--><!--</MarkdownReplacement>-->
+</div>
+
+<div id="Bot-Input">
+<!--<MarkdownReplacement with="competition-Bot-Input.md">--><!--</MarkdownReplacement>-->
+</div>
+
+<div id="Bot-Output">
+<!--<MarkdownReplacement with="competition-Bot-Output.md">--><!--</MarkdownReplacement>-->
+</div>
+
+<div id="Map-Format">
+<!--<MarkdownReplacement with="competition-Map-Format.md">--><!--</MarkdownReplacement>-->
+</div>
+
+<div id="Replay-Format">
+<!--<MarkdownReplacement with="competition-Replay-Format.md">--><!--</MarkdownReplacement>-->
+</div>
+
 <!--<MarkdownReplacement with="competition-Game-Specification.md">--><!--</MarkdownReplacement>-->
 
 <?php
