e8a1b1f4fdd4f8edff7ba59a360bce557153fe9a
McLeopold <mcleopold@gmail.com>
2011-09-30 09:36:39 -0700

    change to map parsing to support scenarios

--- ants/ants.py
+++ ants/ants.py
@@ -189,7 +189,7 @@ class Ants(Game):
 
     def parse_map(self, map_text):
         """ Parse the map_text into a more friendly data structure """
-        players = []
+        ant_list = None
         hill_list = []
         hill_count = defaultdict(int)
         width = height = None
@@ -198,6 +198,9 @@ class Ants(Game):
         ants = defaultdict(list)
         hills = defaultdict(list)
         row = 0
+        score = None
+        hive = None
+        num_players = None
 
         for line in map_text.split('\n'):
             line = line.strip().lower()
@@ -211,36 +214,38 @@ class Ants(Game):
                 width = int(value)
             elif key == 'rows':
                 height = int(value)
+            elif key == 'players':
+                num_players = int(value)
+                if num_players < 2 or num_players > 10:
+                    raise Exception("map",
+                                    "player count must be between 2 and 10")
+            elif key == 'score':
+                score = map(int, value.split())
+            elif key == 'hive':
+                hive = map(int, value.split())
             elif key == 'm':
+                if ant_list is None:
+                    if num_players is None:
+                        raise Exception("map",
+                                        "players count expected before map lines")
+                    ant_list = [chr(97 + i) for i in range(num_players)]
+                    hill_list = map(str, range(num_players))
+                    hill_ant = [chr(65 + i) for i in range(num_players)]
                 if len(value) != width:
                     raise Exception("map",
                                     "Incorrect number of cols in row %s. "
                                     "Got %s, expected %s."
                                     %(row, len(value), width))
                 for col, c in enumerate(value):
-                    # assign player ids in the order that we see them
-                    #  (so player 'a' won't necessarily be 0, and so on)
-                    # look for ants or hills or ants on hills
-                    if c in PLAYER_ANT:
-                        if c not in players:
-                            players.append(c)
-                            hill_list.append(PLAYER_HILL[PLAYER_ANT.index(c)])
-                            hill_count[players.index(c)] = 0
-                        ants[players.index(c)].append((row,col))
-                    elif c in PLAYER_HILL:
-                        if c not in hill_list:
-                            hill_list.append(c)
-                            players.append(PLAYER_ANT[PLAYER_HILL.index(c)])
+                    if c in ant_list:
+                        ants[ant_list.index(c)].append((row,col))
+                    elif c in hill_list:
                         hills[hill_list.index(c)].append((row,col))
                         hill_count[hill_list.index(c)] += 1
-                    elif c in HILL_ANT:
-                        c = PLAYER_ANT[HILL_ANT.index(c)]
-                        if c not in players:
-                            players.append(c)
-                            hill_list.append(PLAYER_HILL[PLAYER_ANT.index(c)])
-                        ants[players.index(c)].append((row,col))
-                        hills[players.index(c)].append((row,col))
-                        hill_count[players.index(c)] += 1
+                    elif c in hill_ant:
+                        ants[hill_ant.index(c)].append((row,col))
+                        hills[hill_ant.index(c)].append((row,col))
+                        hill_count[hill_ant.index(c)] += 1
                     elif c == MAP_OBJECT[FOOD]:
                         food.append((row,col))
                     elif c == MAP_OBJECT[WATER]:
@@ -250,21 +255,31 @@ class Ants(Game):
                                         "Invalid character in map: %s" % c)
                 row += 1
 
+        if score and len(score) != num_players:
+            raise Exception("map",
+                            "Incorrect score count.  Expected %s, got %s"
+                            % (num_players, len(score)))
+        if hive and len(hive) != num_players:
+            raise Exception("map",
+                            "Incorrect score count.  Expected %s, got %s"
+                            % (num_players, len(score)))
+
         if height != row:
             raise Exception("map",
                             "Incorrect number of rows.  Expected %s, got %s"
                             % (height, row))
 
         # look for ants without hills to invalidate map for a game
-        for hill, count in hill_count.items():
-            if count == 0:
-                raise Exception("map",
-                                "Player %s has no starting hills"
-                                % hill)
+        if not self.scenario:
+            for hill, count in hill_count.items():
+                if count == 0:
+                    raise Exception("map",
+                                    "Player %s has no starting hills"
+                                    % hill)
 
         return {
             'size':        (height, width),
-            'num_players': len(players),
+            'num_players': num_players,
             'hills':       hills,
             'ants':        ants,
             'food':        food,
