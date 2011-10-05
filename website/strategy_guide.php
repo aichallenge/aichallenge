1a9f670125f62777fd3958d2fe5888aaa78bfcec
McLeopold <mcleopold@gmail.com>
2011-10-05 14:08:49 -0700

    add more web pages

---
+++ website/strategy_guide.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('header.php');
+
+?>
+
+<!--<MarkdownReplacement with="competition-Strategy-Guide.md">--><!--</MarkdownReplacement>-->
+
+<?php
+
+require_once('visualizer_widget.php');
+visualize_pre();
+require_once('footer.php');
+
+?>
