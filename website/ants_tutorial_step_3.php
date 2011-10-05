92e79b1536345f8a0cc47b098da2e68153a8bd00
McLeopold <mcleopold@gmail.com>
2011-10-05 14:08:18 -0700

    add ants tutorial pages

---
+++ website/ants_tutorial_step_3.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('header.php');
+
+?>
+
+<!--<MarkdownReplacement with="competition-Tutorial-Step-3.md">--><!--</MarkdownReplacement>-->
+
+<?php
+
+require_once('visualizer_widget.php');
+visualize_pre();
+require_once('footer.php');
+
+?>
