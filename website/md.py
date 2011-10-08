c32ea8193aba15634a47f082294c0593e6f79b12
Scott Hamilton <mcleopold@gmail.com>
2011-10-08 08:46:15 -0700

    add python markdown script

---
+++ website/md.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import sys
+import markdown
+md = markdown.Markdown(
+        extensions=['extra',
+                    'codehilite',
+                    'toc',
+                    'wikilinks',
+                    'latex']
+     )
+mdfile = open(sys.argv[1], 'r')
+mdtext = mdfile.read()
+mdfile.close()
+sys.stdout.write(md.convert(mdtext))
+sys.stdout.write('\n');
