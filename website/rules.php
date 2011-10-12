79231c8eb1078d9cb1573428709222b1f560c0fe
janzert <janzert@janzert.com>
2011-10-12 16:42:21 -0400

    Remove time limit disqualification rule since we no longer disqualify on that

--- website/rules.php
+++ website/rules.php
@@ -9,18 +9,13 @@
   <li>You can only have one account. If you have effective control over more
     than one account, even if the accounts are all nominally owned by other
     people, you will be disqualified.</li>
-  <li>Your program may not take more than one second to make any
-    individual move. If you want to do intensive calculations, be sure to add
-    some code that checks the time at regular intervals so you don't go over
-    your one second quota. If your program violates its time quota, it will
-    be automatically suspended.</li>
   <li>Entries which are deemed to violate the spirit of fair and sportsmanlike
     competition will be disqualified without any opportunity for appeal.
     In particular, memory scanning, intentionally losing games, and behavior
     conditional on the opponent's identity are prohibited.</li>
   <li>You may not write to files. You may however read from files within your
     submission directory, which will be the current directory.</li>
-  <li>Use of multiple processes or threads is prohibited.</li>
+  <li>Use of multiple processes or OS threads are prohibited.</li>
   <li>Any attempt to disrupt the normal operation of the contest software or
     the contest servers will result in the immediate involvement of law
     enforcement officials. Our policy is to always prosecute.</li>
