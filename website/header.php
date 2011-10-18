f8beb45c8c5f5108f53b52c91be97ffecec93383
Alexandru Stan <alex@hypertriangle.com>
2011-10-18 18:09:04 -0400

    readded google analytics

--- website/header.php
+++ website/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <?php
+require_once('server_info.php');
 $page_render_start_time = microtime(true);
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml"> 
@@ -19,6 +20,16 @@ $page_render_start_time = microtime(true);
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
         <script src="/js/jquery.tablesorter.min.js"></script>
         <script src="/js/tab_sync.js"></script>
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', '<?php echo $server_info["google_analytics_key"]; ?>']);
+            _gaq.push(['_trackPageview']);
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
     </head> 
 <?php
 require_once('session.php');
