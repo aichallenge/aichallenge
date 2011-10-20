fa5a713efe696b87675e0e0e3c5c0d0632bdc3e4
Alexandru Stan <alex@hypertriangle.com>
2011-10-20 16:35:06 -0400

    relinked forums to be relative

--- website/menu.php
+++ website/menu.php
@@ -36,7 +36,7 @@
     <h1>Help</h1>
     <ul>
       <li><a href="faq.php">Frequently Asked Questions</a></li>
-      <li><a href="http://aichallenge.org/forums/">Forums</a></li>
+      <li><a href="forums/">Forums</a></li>
       <li><a href="irc://irc.freenode.org/aichallenge">IRC</a> <a href="http://webchat.freenode.net/?channels=aichallenge&uio=d4">(webclient)</a></li>
     </ul>
     <h1>User Search:</h1>
