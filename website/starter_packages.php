04e856464eeb6200ed9a93fc68e72c740a1c00ee
janzert <janzert@janzert.com>
2011-11-01 23:04:51 -0400

    Update Scala starter bot status

--- website/starter_packages.php
+++ website/starter_packages.php
@@ -164,7 +164,7 @@ include 'header.php';
     <td><a href="starter_packages/scala_starter_package.zip">Scala</a></td>
     <td>Scala version 2.9.0.1</td>
     <td>&nbsp;</td>
-    <td><em>Hills not implemented yet</em></td>
+    <td>&nbsp;</td>
   </tr>
   <tr>
     <td><a href="starter_packages/tcl_starter_package.zip">Tcl</a></td>
