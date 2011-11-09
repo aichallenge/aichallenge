b73ea8cd6674d1832e90c16cb8cfd8028d87e84b
Alexandru Stan <alex@hypertriangle.com>
2011-11-09 15:18:28 -0500

    removed csclub link

--- website/organization_rankings.php
+++ website/organization_rankings.php
@@ -5,7 +5,6 @@ include_once 'organization_rankings_widget.php';
 echo <<<EOT
 <h2>Organizations by Participation</h2>
 <p>Here are the participating organizations ranked by number of entries in the top 100.</p>
-<p>If you want to sign up your company or school, simply post in <a href="http://csclub.uwaterloo.ca/contest/forums/viewtopic.php?f=4&t=17">this forum thread</a>.
 EOT;
 
 echo getOrganizationRankingsTableString(100);
