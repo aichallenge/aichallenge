d7339b5349988ba7388ae09ac3f5c587aaf855fb
janzert <janzert@janzert.com>
2011-11-11 18:52:44 -0500

    Update and clarify contest rules

--- website/rules.php
+++ website/rules.php
@@ -15,15 +15,20 @@ include 'header.php'
     than one account, even if the accounts are all nominally owned by other
     people, you will be disqualified.</li>
   <li>Entries which are deemed to violate the spirit of fair and sportsmanlike
-    competition will be disqualified without any opportunity for appeal.
-    In particular, memory scanning, intentionally losing games, and behavior
-    conditional on the opponent's identity are prohibited.</li>
-  <li>You may not write to files. You may however read from files within your
-    submission directory, which will be the current directory.</li>
-  <li>Use of multiple processes or OS threads are prohibited.</li>
-  <li>Any attempt to disrupt the normal operation of the contest software or
-    the contest servers will result in the immediate involvement of law
-    enforcement officials. Our policy is to always prosecute.</li>
+    competition will be disqualified without any opportunity for appeal. In
+    particular, memory scanning, manipulating rankings by intentionally losing
+    games, and trying to gather information about your opponent or the game
+    state through means outside of the bot input protocol are prohibited.</li>
+  <li>Data written to files will not be preserved between games and a bot found
+    to be writing excessive amounts (more than 10MB) will be disqualified.</li>
+  <li>Actively using multiple processes or OS threads is not allowed. Some
+    language runtimes will not operate without using multiple OS threads so we
+    allow the creation of a limited number. But a bot found to be running
+    active computation on more than one will be disqualified.</li>
+  <li>As one would hope should not even need mentioning; any attempt to bypass
+    or disable the contest's security measures, disrupt the contest software or
+    servers, or otherwise interfere with normal operation of the contest is of
+    course prohibited.</li>
   <li>We reserve the right to change these rules at any time without notice.</li>
 </ul>
 
