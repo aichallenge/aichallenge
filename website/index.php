ad417793789c354a12b7e36f503c1bd101801cfa
Alexandru Stan <alex@hypertriangle.com>
2011-11-16 09:14:02 -0500

    removed wiki stuff from index

--- website/index.php
+++ website/index.php
@@ -11,12 +11,7 @@ if(file_exists('server_message.html')) {
 
 ?>
 
-<!--<MarkdownReplacement with="competition.md">--><h1>Introducing Ants</h1>
-
-<p>The Google AI Challenge is all about creating artificial intelligence, whether you are a beginning programmer or an expert. Using one of the easy-to-use starter kits, you will create a computer program (in any language) that controls a colony of ants which fight against other colonies for domination.</p>
-
-<p>It only takes 5 minutes to submit one of the starter kits to the website and watch your ant colony fight for domination against colonies created by other people from around the world. From there check out the tutorials on how to locally run your bot and begin programming!</p>
-<!--</MarkdownReplacement>-->
+<!--<MarkdownReplacement with="competition.md">--><!--</MarkdownReplacement>-->
 
 <p>Computer Programs Duking it Out with Ants:</p>
 <?php
