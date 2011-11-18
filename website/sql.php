aad55a612020d9bf980d5628d05f2af959b51a9c
janzert <janzert@janzert.com>
2011-11-18 01:46:23 -0500

    Now remove the extra comma

--- website/sql.php
+++ website/sql.php
@@ -244,7 +244,7 @@ $contest_sql = array(
          inner join submission s
             on gp.submission_id = s.submission_id
          order by g.game_id desc, gp.game_rank",
-    "select_game_errors" => "select gp.user_id, gp.errors, gp.status, u.username,
+    "select_game_errors" => "select gp.user_id, gp.errors, gp.status, u.username
         from game_player gp
         inner join user u on u.user_id == gp.user_id
         where gp.game_id = %s
