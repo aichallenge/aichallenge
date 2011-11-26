9f1a828bc49c86e0f243d392ff029b4f78f388dc
Robert Grosse <n210241048576@gmail.com>
2011-11-25 20:06:04 -0500

    Fix timer
    Avoid exposing system headers to rest of bot, switch to QueryPerformanceCounter on Windows, and add const correctness.

--- ants/dist/starter_bots/cpp/Timer.h
+++ ants/dist/starter_bots/cpp/Timer.h
@@ -1,64 +1,20 @@
-#ifndef TIMER_H_
-#define TIMER_H_
+#pragma once
 
-#include <sys/time.h>
+struct Timer_DataMembersStruct;
 
-/*
-    struct for checking how long it has been since the start of the turn.
-*/
-#ifdef _WIN32 //Windows timer (DON'T USE THIS TIMER UNLESS YOU'RE ON WINDOWS!)
-    #include <io.h>
-    #include <windows.h>
+struct Timer
+{
+    Timer();
+    ~Timer();
 
-    struct Timer
-    {
-        clock_t startTime, currentTime;
+    void start();
+    double getTime() const;
 
-        Timer()
-        {
+    private:
+    Timer_DataMembersStruct* opaque;
 
-        };
+    //Since it contains a pointer, it must be noncopyable
+    Timer(const Timer& other);
+    Timer& operator=(const Timer& other);
+};
 
-        void start()
-        {
-            startTime = clock();
-        };
-
-        double getTime()
-        {
-            currentTime = clock();
-
-            return (double)(currentTime - startTime);
-        };
-    };
-
-#else //Mac/Linux Timer
-    struct Timer
-    {
-        timeval timer;
-        double startTime, currentTime;
-
-        Timer()
-        {
-
-        };
-
-        //starts the timer
-        void start()
-        {
-            gettimeofday(&timer, NULL);
-            startTime = timer.tv_sec+(timer.tv_usec/1000000.0);
-        };
-
-        //returns how long it has been since the timer was last started in milliseconds
-        double getTime()
-        {
-            gettimeofday(&timer, NULL);
-            currentTime = timer.tv_sec+(timer.tv_usec/1000000.0);
-            return (currentTime-startTime)*1000.0;
-        };
-    };
-#endif
-
-
-#endif //TIMER_H_
