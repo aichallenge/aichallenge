4e4705d5a652712ae4e1bb20e084c3fcee986bf4
McLeopold <mcleopold@gmail.com>
2011-12-21 09:49:17 -0800

    add countdown desc to server_info

--- website/header.php
+++ website/header.php
@@ -71,7 +71,7 @@ if ($server_info["show_countdown"]) {
         	<div class="countdown" id="countdown">Timer Goes Here</div>
 			<script>
 				$('#countdown').countdown({until: new Date('<?php $now = new DateTime($server_info['deadline']); echo $now->format("M j, Y H:i:s O"); ?>'),
-					                       description: <?php $server_info['deadline_desc']; ?>});
+					                       description: <?php echo $server_info['deadline_desc']; ?>});
 			</script>
 <?php 
 }
